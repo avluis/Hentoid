1a83a175c8d1c9ada2bc5d63485f881558db90c3
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2015-12-29 23:41:59 +0800

    Replaced menu overflow with toggle button
    Also added icons and replaced search icon

--- app/src/main/java/me/devsaki/hentoid/DownloadsActivity.java
+++ app/src/main/java/me/devsaki/hentoid/DownloadsActivity.java
@@ -42,8 +42,7 @@ public class DownloadsActivity extends HentoidActivity<DownloadsActivity.Downloa
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_content_list, menu);
+        getMenuInflater().inflate(R.menu.menu_content_list, menu);
 
         // Associate searchable configuration with the SearchView
         SearchManager searchManager =
