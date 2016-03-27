28c7ff6f2bc9fae7a974163da4186656c5aeb318
Luis E Alvarado <alvaradorocks@gmail.com>
2016-03-27 17:21:50 -0500

    Initial test implementation

--- app/src/main/java/me/devsaki/hentoid/activities/IntroSlideActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/IntroSlideActivity.java
@@ -1,8 +1,11 @@
 package me.devsaki.hentoid.activities;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 
 import com.github.paolorotolo.appintro.AppIntro2;
@@ -13,9 +16,12 @@ import me.devsaki.hentoid.util.AndroidHelper;
 
 /**
  * Created by avluis on 03/20/2016.
- * Introduction activity
+ * Welcome activity
+ * Presents required permissions, sets storage directory and library import
+ * Finish implementing: TODO: File Picker, Library Import
  */
 public class IntroSlideActivity extends AppIntro2 {
+    private int importSlide = 4;
 
     @Override
     public void init(@Nullable Bundle savedInstanceState) {
@@ -33,6 +39,7 @@ public class IntroSlideActivity extends AppIntro2 {
         addSlide(BaseSlide.newInstance(R.layout.intro_slide_03));
         addSlide(BaseSlide.newInstance(R.layout.intro_slide_04));
         addSlide(BaseSlide.newInstance(R.layout.intro_slide_05));
+        addSlide(BaseSlide.newInstance(R.layout.intro_slide_06));
     }
 
     @Override
@@ -50,9 +57,46 @@ public class IntroSlideActivity extends AppIntro2 {
     @Override
     public void onSlideChanged() {
         // Show the import dialog just prior to the last slide
-        int importSlide = 4;
         if (pager.getCurrentItem() == importSlide) {
             System.out.println("Is this the right slide?");
+
+            Handler handler = new Handler();
+
+            handler.postDelayed(new Runnable() {
+
+                public void run() {
+                    Intent selectFolder = new Intent(
+                            getApplicationContext(), SelectFolderActivity2.class);
+                    startActivityForResult(selectFolder, 1);
+                }
+            }, 100);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                // If we get RESULT_OK, then we validate
+                // TODO: Implement result validation
+                System.out.println("RESULT_OK: ");
+                String result = data.getStringExtra("result");
+                System.out.println(result);
+
+                /* If result passes validation, then:
+                * TODO: If library path contains prior downloads, we import then move to next slide
+                * TODO: If library path is clean, then we simply move to next slide
+                */
+
+                // Move to next slide
+                pager.setCurrentItem(importSlide + 1);
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                System.out.println("RESULT_CANCELED");
+                // If we get RESULT_CANCELED, then go back 2 slides
+                pager.setCurrentItem(importSlide - 2);
+            }
+
         }
     }
 }
