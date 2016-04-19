780bc2f0fed3cd59718018ae0c874611170107b8
Luis E Alvarado <alvaradorocks@gmail.com>
2016-04-19 11:43:36 -0500

    Enabled Strict Mode for debug builds to assist with refactoring.

--- app/src/main/java/me/devsaki/hentoid/fragments/DownloadsFragment.java
+++ app/src/main/java/me/devsaki/hentoid/fragments/DownloadsFragment.java
@@ -9,6 +9,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.MenuItemCompat;
@@ -28,6 +29,7 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.devsaki.hentoid.BuildConfig;
 import me.devsaki.hentoid.HentoidApplication;
 import me.devsaki.hentoid.R;
 import me.devsaki.hentoid.abstracts.BaseFragment;
@@ -373,6 +375,30 @@ public class DownloadsFragment extends BaseFragment implements DrawerLayout.Draw
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        if (BuildConfig.DEBUG) {
+            // StrictMode to assist with refactoring
+            /** {@link StrictMode.ThreadPolicy},
+             * {@link StrictMode.ThreadPolicy.Builder#detectDiskReads()},
+             * {@link StrictMode.ThreadPolicy.Builder#detectDiskWrites()},
+             * {@link StrictMode.ThreadPolicy.Builder#detectNetwork()}
+             */
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .penaltyLog()
+                    .build());
+            /** {@link StrictMode.VmPolicy},
+             * {@link StrictMode.VmPolicy.Builder#detectLeakedSqlLiteObjects()},
+             * {@link StrictMode.VmPolicy.Builder#detectLeakedClosableObjects()}
+             */
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
         super.onCreate(savedInstanceState);
 
         setHasOptionsMenu(true);
