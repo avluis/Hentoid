113a5684f36dbe4db1e10e3f01f2b1d9f7e4e8f7
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2016-11-09 21:48:17 +0800

    Minor cleanup (#129)
    * Reimplemented AnalyticsTrackers to purely static instead of Singleton:
        simplifies code
        fixes memory leak for mContext
    * Merge exclusive method in LogHelper

--- app/src/main/java/me/devsaki/hentoid/AnalyticsTrackers.java
+++ app/src/main/java/me/devsaki/hentoid/AnalyticsTrackers.java
@@ -1,6 +1,5 @@
 package me.devsaki.hentoid;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
@@ -12,43 +11,22 @@ import java.util.Map;
 /**
  * Created by avluis on 2/11/16.
  * A collection of Google Analytics trackers. Fetch the tracker you need using
- * {@code AnalyticsTrackers.getInstance().get(...)}
+ * {@code AnalyticsTrackers.get(...)}
  */
 final class AnalyticsTrackers {
 
-    @SuppressLint("StaticFieldLeak")
-    private static AnalyticsTrackers sInstance;
-    private final Map<Target, Tracker> mTrackers = new HashMap<>();
-    private final Context mContext;
+    private static final Map<Target, Tracker> mTrackers = new HashMap<>();
 
-    /**
-     * Don't instantiate directly - use {@link #getInstance()} instead.
-     */
-    private AnalyticsTrackers(Context context) {
-        mContext = context.getApplicationContext();
+    private AnalyticsTrackers() {
+        throw new AssertionError("Do not instantiate");
     }
 
-    static synchronized void initialize(Context context) {
-        if (sInstance != null) {
-            throw new IllegalStateException("Extra call to initialize analytics trackers");
-        }
-        sInstance = new AnalyticsTrackers(context);
-    }
-
-    public static synchronized AnalyticsTrackers getInstance() {
-        if (sInstance == null) {
-            throw new IllegalStateException("Call initialize() before getInstance()");
-        }
-
-        return sInstance;
-    }
-
-    public synchronized Tracker get(Target target) {
+    static synchronized Tracker get(Context context, Target target) {
         if (!mTrackers.containsKey(target)) {
             Tracker tracker;
             switch (target) {
                 case APP:
-                    tracker = GoogleAnalytics.getInstance(mContext).newTracker(R.xml.app_tracker);
+                    tracker = GoogleAnalytics.getInstance(context).newTracker(R.xml.app_tracker);
                     break;
                 default:
                     throw new IllegalArgumentException("Unhandled analytics target " + target);
--- app/src/main/java/me/devsaki/hentoid/util/LogHelper.java
+++ app/src/main/java/me/devsaki/hentoid/util/LogHelper.java
@@ -26,19 +26,16 @@ public class LogHelper {
     private static final int LOG_PREFIX_LENGTH = LOG_PREFIX.length();
     private static final int MAX_LOG_TAG_LENGTH = 23;
 
-    private static String makeLogTag(String str) {
-        if (str.length() > MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH) {
-            return LOG_PREFIX + str.substring(0, MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH - 1);
-        }
-
-        return LOG_PREFIX + str;
-    }
-
     /**
      * Don't use this when obfuscating class names!
      */
     public static String makeLogTag(Class cls) {
-        return makeLogTag(cls.getSimpleName());
+        final String className = cls.getSimpleName();
+        if (className.length() > MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH) {
+            return LOG_PREFIX + className.substring(0, MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH - 1);
+        }
+
+        return LOG_PREFIX + className;
     }
 
     public static void v(String tag, Object... messages) {
