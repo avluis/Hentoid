eec42a314310de5a8725080d3dff2b7f3880069a
KingRikkie <despo2015@yandex.com>
2018-02-21 19:53:33 +0100

    Added basic adblocker for tsumino

--- app/src/main/java/me/devsaki/hentoid/activities/TsuminoActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/TsuminoActivity.java
@@ -99,7 +99,7 @@ public class TsuminoActivity extends BaseWebActivity {
         @Override
         public WebResourceResponse shouldInterceptRequest(@NonNull WebView view,
                                                           @NonNull String url) {
-            if (url.contains("pop.js")) {
+            if (url.contains("pop.js") || url.contains("/static/")) {
                 return new WebResourceResponse("text/plain", "utf-8", nothing);
             } else {
                 return super.shouldInterceptRequest(view, url);
@@ -111,7 +111,7 @@ public class TsuminoActivity extends BaseWebActivity {
         public WebResourceResponse shouldInterceptRequest(@NonNull WebView view,
                                                           @NonNull WebResourceRequest request) {
             String url = request.getUrl().toString();
-            if (url.contains("pop.js")) {
+            if (url.contains("pop.js") || url.contains("/static/")) {
                 return new WebResourceResponse("text/plain", "utf-8", nothing);
             } else {
                 return super.shouldInterceptRequest(view, request);
