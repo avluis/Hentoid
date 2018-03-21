ba5a47714f64b42bd80f26fac6743c3dbfdf1c01
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2018-03-21 13:41:00 +0800

    modernized AboutActivity

--- app/src/main/java/me/devsaki/hentoid/activities/AboutActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/AboutActivity.java
@@ -1,23 +1,24 @@
 package me.devsaki.hentoid.activities;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AlertDialog;
-import android.text.Spanned;
+import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import me.devsaki.hentoid.BuildConfig;
 import me.devsaki.hentoid.R;
 import me.devsaki.hentoid.abstracts.BaseActivity;
-import me.devsaki.hentoid.util.Helper;
+
+import static android.content.Intent.ACTION_VIEW;
 
 /**
- * Created by avluis on 8/22/15.
- * Presents an About Screen for the user to inquire more about the app.
+ * Created by wightwulf1944 on 03/21/18.
  */
 public class AboutActivity extends BaseActivity {
 
@@ -26,108 +27,38 @@ public class AboutActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_about);
-        setTitle(R.string.title_activity_about);
 
-        final Intent intent = new Intent(Intent.ACTION_VIEW);
-        populateLinks(intent);
-        attachBuildNotes();
-        attachLicenseNotes();
+        bindTextViewLink(R.id.iv_app_logo, R.string.about_github_wiki_url);
+        bindTextViewLink(R.id.tv_github, R.string.about_github_url);
+        bindTextViewLink(R.id.tv_community, R.string.about_community_url);
+        bindTextViewLink(R.id.tv_discord, R.string.about_discord_url);
+        bindTextViewLink(R.id.tv_reddit, R.string.about_reddit_url);
+
+        TextView tvVersionName = findViewById(R.id.tv_version_name);
+        tvVersionName.setText("Hentoid ver: " + BuildConfig.VERSION_NAME);
+
+        WebView webView = new WebView(this);
+        webView.loadUrl("file:///android_asset/licenses.html");
+        webView.setInitialScale(95);
+
+        AlertDialog licensesDialog = new AlertDialog.Builder(this)
+                .setTitle("Licenses")
+                .setView(webView)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+
+        // TODO: dialog should not show large content or a no-op button
+        // replace with activity instead
+        Button btnLicenses = findViewById(R.id.btn_about_licenses);
+        btnLicenses.setOnClickListener(view -> licensesDialog.show());
     }
 
-    private void populateLinks(final Intent intent) {
-        ImageView ivAppLogo = (ImageView) findViewById(R.id.iv_app_logo);
-        final String urlGitHubWiki = getString(R.string.about_github_wiki_url);
-        if (ivAppLogo != null) {
-            ivAppLogo.setOnClickListener(v -> {
-                intent.setData(Uri.parse(urlGitHubWiki));
-                startActivity(intent);
-            });
-        }
+    private void bindTextViewLink(@IdRes int tvId, @StringRes int linkId) {
+        String url = getString(linkId);
+        Uri uri = Uri.parse(url);
+        Intent intent = new Intent(ACTION_VIEW, uri);
 
-        Spanned spGitHub = Helper.fromHtml(getString(R.string.about_github));
-        TextView tvGitHub = (TextView) findViewById(R.id.tv_github);
-        final String urlGitHub = getString(R.string.about_github_url);
-        if (tvGitHub != null) {
-            tvGitHub.setText(spGitHub);
-            tvGitHub.setOnClickListener(v -> {
-                intent.setData(Uri.parse(urlGitHub));
-                startActivity(intent);
-            });
-        }
-
-        Spanned spCommunity = Helper.fromHtml(getString(R.string.about_community));
-        TextView tvCommunity = (TextView) findViewById(R.id.tv_community);
-        final String urlCommunity = getString(R.string.about_community_url);
-        if (tvCommunity != null) {
-            tvCommunity.setText(spCommunity);
-            tvCommunity.setOnClickListener(v -> {
-                intent.setData(Uri.parse(urlCommunity));
-                startActivity(intent);
-            });
-        }
-
-        Spanned spDiscord = Helper.fromHtml(getString(R.string.about_discord));
-        TextView tvDiscord = (TextView) findViewById(R.id.tv_discord);
-        final String urlBlog = getString(R.string.about_discord_url);
-        if (tvDiscord != null) {
-            tvDiscord.setText(spDiscord);
-            tvDiscord.setOnClickListener(v -> {
-                intent.setData(Uri.parse(urlBlog));
-                startActivity(intent);
-            });
-        }
-
-        Spanned spReddit = Helper.fromHtml(getString(R.string.about_reddit));
-        TextView tvReddit = (TextView) findViewById(R.id.tv_reddit);
-        final String urlReddit = getString(R.string.about_reddit_url);
-        if (tvReddit != null) {
-            tvReddit.setText(spReddit);
-            tvReddit.setOnClickListener(v -> {
-                intent.setData(Uri.parse(urlReddit));
-                startActivity(intent);
-            });
-        }
-    }
-
-    private void attachBuildNotes() {
-        Spanned spAbout = Helper.fromHtml(getString(R.string.about));
-        TextView tvAbout = (TextView) findViewById(R.id.tv_about);
-        if (tvAbout != null) {
-            tvAbout.setText(spAbout);
-        }
-
-        String verName = "Hentoid ver: ";
-        try {
-            verName += Helper.getAppVersionInfo(this);
-        } catch (PackageManager.NameNotFoundException e) {
-            verName += "Unknown";
-        }
-
-        TextView tvVersionName = (TextView) findViewById(R.id.tv_version_name);
-        if (tvVersionName != null) {
-            tvVersionName.setText(verName);
-        }
-
-        Spanned spAboutNotes = Helper.fromHtml(getString(R.string.about_notes));
-        TextView tvAboutNotes = (TextView) findViewById(R.id.tv_about_notes);
-        if (tvAboutNotes != null) {
-            tvAboutNotes.setText(spAboutNotes);
-        }
-    }
-
-    private void attachLicenseNotes() {
-        Button btnLicenses = (Button) findViewById(R.id.btn_about_licenses);
-        btnLicenses.setOnClickListener(view -> {
-            final AlertDialog.Builder alert = new AlertDialog.Builder(AboutActivity.this);
-            alert.setTitle("Licenses");
-
-            final WebView wv = new WebView(AboutActivity.this);
-            wv.loadUrl("file:///android_asset/licenses.html");
-            wv.setInitialScale(95);
-
-            alert.setView(wv);
-            alert.setNegativeButton(android.R.string.ok, (dialog, id) -> dialog.dismiss());
-            alert.show();
-        });
+        View linkableView = findViewById(tvId);
+        linkableView.setOnClickListener(v -> startActivity(intent));
     }
 }
