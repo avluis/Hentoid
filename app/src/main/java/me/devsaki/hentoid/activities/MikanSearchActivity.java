3cf07a143763b8e91f2e0ecd255320ebaabf89b9
Robb <35880555+RobbWatershed@users.noreply.github.com>
2018-06-29 23:02:54 +0200

    Initial setup for Mikan activity

---
+++ app/src/main/java/me/devsaki/hentoid/activities/MikanSearchActivity.java
@@ -0,0 +1,20 @@
+package me.devsaki.hentoid.activities;
+
+import android.os.Bundle;
+
+import me.devsaki.hentoid.R;
+import me.devsaki.hentoid.abstracts.BaseActivity;
+
+/**
+ * Created by Robb on 06/2018
+ */
+public class MikanSearchActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_mikan);
+    }
+
+}
