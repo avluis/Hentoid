b63d09323960027088ef7fcbdd3c51f764cc766c
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2018-07-07 00:03:08 +0800

    Fixed new user experience for RTL locales

---
+++ app/src/main/java/me/devsaki/hentoid/fragments/intro/WelcomeIntroFragment.java
@@ -0,0 +1,20 @@
+package me.devsaki.hentoid.fragments.intro;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.devsaki.hentoid.R;
+
+public class WelcomeIntroFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.intro_slide_01, container, false);
+    }
+}
