6ef5ed7f8c61db4947aa53ddc5f00ef10cce82f7
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2018-07-21 22:22:36 +0800

    Fixed crash on android versions that don't support File.toPath()

--- app/src/main/java/me/devsaki/hentoid/util/FileHelper.java
+++ app/src/main/java/me/devsaki/hentoid/util/FileHelper.java
@@ -196,12 +196,25 @@ public class FileHelper {
      */
     static boolean cleanDirectory(@NonNull File target) {
         try {
-            FileUtils.cleanDirectory(target);
-            return true;
-        } catch (IOException e) {
+            return tryCleanDirectory(target);
+        } catch (Exception e) {
             Timber.e(e, "Failed to clean directory");
             return false;
         }
+    }
+
+    private static boolean tryCleanDirectory(@NonNull File directory) throws IOException, SecurityException {
+        File[] files = directory.listFiles();
+        if (files == null) throw new IOException("Failed to list contents of " + directory);
+
+        boolean isSuccess = true;
+
+        for (File file : files) {
+            if (file.isDirectory()) tryCleanDirectory(file);
+            if (!file.delete() && file.exists()) isSuccess = false;
+        }
+
+        return isSuccess;
     }
 
     public static boolean validateFolder(String folder) {
