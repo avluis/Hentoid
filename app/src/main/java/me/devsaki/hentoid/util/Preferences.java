d2788d5c249258a12311dbead8935b01444a9562
Robb <35880555+RobbWatershed@users.noreply.github.com>
2018-10-10 21:53:11 +0200

    Author - Title - ID now is the default folder naming pattern

--- app/src/main/java/me/devsaki/hentoid/util/Preferences.java
+++ app/src/main/java/me/devsaki/hentoid/util/Preferences.java
@@ -206,7 +206,7 @@ public final class Preferences {
         static final boolean PREF_APP_LOCK_VIBRATE_DEFAULT = true;
         static final boolean PREF_ENDLESS_SCROLL_DEFAULT = true;
         static final boolean PREF_HIDE_RECENT_DEFAULT = true;
-        static final int PREF_FOLDER_NAMING_CONTENT_DEFAULT = Constant.PREF_FOLDER_NAMING_CONTENT_ID;
+        static final int PREF_FOLDER_NAMING_CONTENT_DEFAULT = Constant.PREF_FOLDER_NAMING_CONTENT_AUTH_TITLE_ID;
         static final int PREF_READ_CONTENT_ACTION = Constant.PREF_READ_CONTENT_DEFAULT;
         static final boolean PREF_CHECK_UPDATES_DEFAULT = true;
         static final boolean PREF_WEBVIEW_OVERRIDE_OVERVIEW_DEFAULT = false;
