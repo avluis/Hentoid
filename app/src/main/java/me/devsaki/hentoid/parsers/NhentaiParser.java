91a3a13e2759843f44c8b6d50daf84e6eb74d0ba
Robb <35880555+RobbWatershed@users.noreply.github.com>
2019-01-14 22:43:24 +0100

    Alternate, more stable version of the Nhentai HTML parser

--- app/src/main/java/me/devsaki/hentoid/parsers/NhentaiParser.java
+++ app/src/main/java/me/devsaki/hentoid/parsers/NhentaiParser.java
@@ -20,6 +20,15 @@ public class NhentaiParser extends BaseParser {
     protected List<String> parseImages(Content content) throws IOException {
         List<String> result = new ArrayList<>();
 
+        String[] coverParts = content.getCoverImageUrl().split("/");
+        String mediaId = coverParts[coverParts.length - 2];
+        String imageExt = FileHelper.getExtension(content.getCoverImageUrl());
+        String serverUrl = "https://i.nhentai.net/galleries/" + mediaId + "/";
+
+        for (int i = 0; i < content.getQtyPages(); i++) {
+            result.add(serverUrl + (i + 1) + "." + imageExt); // We infer the whole book is stored on the same server and has the same image format as its cover (both are verified as of 2019/01/14)
+        }
+/*
         String imageUrl = "";
         String pageUrl = content.getReaderUrl() + "1/";
         Document doc = getOnlineDocument(pageUrl);
@@ -33,6 +42,7 @@ public class NhentaiParser extends BaseParser {
         for (int i = 0; i < content.getQtyPages(); i++) {
             result.add(imageUrlPrefix + (i + 1) + "." + imageExt); // We infer the whole book is stored on the same server
         }
+*/
 
         return result;
     }
