e2da5152e22f01b89b0691d24c63aec2d1f4d98f
Robb <35880555+RobbWatershed@users.noreply.github.com>
2019-05-12 21:42:13 +0200

    Fix to volume key listening (#295) with all the behaviour implemented in the same class

--- app/src/main/java/me/devsaki/hentoid/widget/VolumeKeyListener.java
+++ app/src/main/java/me/devsaki/hentoid/widget/VolumeKeyListener.java
@@ -3,11 +3,18 @@ package me.devsaki.hentoid.widget;
 import android.view.KeyEvent;
 import android.view.View;
 
+import java.util.Calendar;
+
 public final class VolumeKeyListener implements View.OnKeyListener {
+
+    private static int ACTION_DELAY_MS = 1000;
 
     private Runnable onVolumeDownKeyListener;
 
     private Runnable onVolumeUpKeyListener;
+
+    private long lastKeyDownEventTick = -1;
+    private boolean isTurbo = false;
 
     public VolumeKeyListener setOnVolumeDownKeyListener(Runnable onVolumeDownKeyListener) {
         this.onVolumeDownKeyListener = onVolumeDownKeyListener;
@@ -22,11 +29,31 @@ public final class VolumeKeyListener implements View.OnKeyListener {
     @Override
     public boolean onKey(View v, int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
-            onVolumeDownKeyListener.run();
-            return true;
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                long timeNow = Calendar.getInstance().getTimeInMillis();
+                if (timeNow - lastKeyDownEventTick > ACTION_DELAY_MS / (isTurbo ? 2 : 1)) {
+                    if (-1 == lastKeyDownEventTick) isTurbo = true;
+                    lastKeyDownEventTick = timeNow;
+                    onVolumeDownKeyListener.run();
+                }
+                return true;
+            } else if (event.getAction() == KeyEvent.ACTION_UP) {
+                lastKeyDownEventTick = -1;
+                isTurbo = false;
+            }
         } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
-            onVolumeUpKeyListener.run();
-            return true;
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                long timeNow = Calendar.getInstance().getTimeInMillis();
+                if (timeNow - lastKeyDownEventTick > ACTION_DELAY_MS / (isTurbo ? 2 : 1)) {
+                    if (-1 == lastKeyDownEventTick) isTurbo = true;
+                    lastKeyDownEventTick = timeNow;
+                    onVolumeUpKeyListener.run();
+                }
+                return true;
+            } else if (event.getAction() == KeyEvent.ACTION_UP) {
+                lastKeyDownEventTick = -1;
+                isTurbo = false;
+            }
         }
         return false;
     }
