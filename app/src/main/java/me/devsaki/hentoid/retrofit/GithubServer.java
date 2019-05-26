855a6fd301803dbed9e0ef4d1d7fdf75f86efaca
Robb <35880555+RobbWatershed@users.noreply.github.com>
2019-05-26 22:31:25 +0200

    Use OkHttp

--- app/src/main/java/me/devsaki/hentoid/retrofit/GithubServer.java
+++ app/src/main/java/me/devsaki/hentoid/retrofit/GithubServer.java
@@ -3,6 +3,7 @@ package me.devsaki.hentoid.retrofit;
 import java.util.List;
 
 import io.reactivex.Single;
+import me.devsaki.hentoid.util.OkHttpClientSingleton;
 import me.devsaki.hentoid.viewholders.GitHubRelease;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -15,6 +16,7 @@ public class GithubServer {
 
     public static final Api API = new Retrofit.Builder()
             .baseUrl(GITHUB_BASE_URL)
+            .client(OkHttpClientSingleton.getInstance())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
