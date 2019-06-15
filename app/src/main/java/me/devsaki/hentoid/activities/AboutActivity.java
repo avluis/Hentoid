2cb1a12f06a693d51b7e3853c16a445a8f08814c
Robb <35880555+RobbWatershed@users.noreply.github.com>
2019-06-15 22:40:01 +0200

    Flag the "View changelog" link label when an update is detected

--- app/src/main/java/me/devsaki/hentoid/activities/AboutActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/AboutActivity.java
@@ -5,9 +5,14 @@ import android.support.annotation.IdRes;
 import android.view.View;
 import android.widget.TextView;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
 import me.devsaki.hentoid.BuildConfig;
 import me.devsaki.hentoid.R;
 import me.devsaki.hentoid.abstracts.BaseActivity;
+import me.devsaki.hentoid.events.UpdateEvent;
 import me.devsaki.hentoid.fragments.about.ChangelogFragment;
 import me.devsaki.hentoid.fragments.about.LicensesFragment;
 import me.devsaki.hentoid.util.Consts;
@@ -17,6 +22,8 @@ import me.devsaki.hentoid.util.Helper;
  * Created by wightwulf1944 on 03/21/18.
  */
 public class AboutActivity extends BaseActivity {
+
+    TextView btnChangelog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +39,13 @@ public class AboutActivity extends BaseActivity {
         TextView tvVersionName = findViewById(R.id.tv_version_name);
         tvVersionName.setText(String.format("Hentoid ver: %s (%s)", BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE));
 
-        View btnChangelog = findViewById(R.id.about_changelog_button);
+        btnChangelog = findViewById(R.id.about_changelog_button);
         btnChangelog.setOnClickListener(v -> showChangelogFragment());
 
         View btnLicenses = findViewById(R.id.about_licenses_button);
         btnLicenses.setOnClickListener(v -> showLicenseFragment());
+
+        if (!EventBus.getDefault().isRegistered(this)) EventBus.getDefault().register(this);
     }
 
     private void bindTextViewLink(@IdRes int tvId, String url) {
@@ -58,5 +67,16 @@ public class AboutActivity extends BaseActivity {
                 .add(android.R.id.content, new ChangelogFragment())
                 .addToBackStack(null) // This triggers a memory leak in LeakCanary but is _not_ a leak : see https://stackoverflow.com/questions/27913009/memory-leak-in-fragmentmanager
                 .commit();
+    }
+
+    @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
+    public void onUpdateEvent(UpdateEvent event) {
+        if (event.hasNewVersion) btnChangelog.setText(R.string.view_changelog_flagged);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (EventBus.getDefault().isRegistered(this)) EventBus.getDefault().unregister(this);
     }
 }
