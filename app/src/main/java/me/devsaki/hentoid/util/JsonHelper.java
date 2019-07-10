9b001dd35ee781373b0610fdae6f0923f6104a53
Robb <35880555+RobbWatershed@users.noreply.github.com>
2019-07-10 22:59:36 +0200

    Remove obsolete methods

--- app/src/main/java/me/devsaki/hentoid/util/JsonHelper.java
+++ app/src/main/java/me/devsaki/hentoid/util/JsonHelper.java
@@ -5,26 +5,15 @@ import androidx.documentfile.provider.DocumentFile;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.nio.charset.Charset;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
-import okhttp3.Call;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 import timber.log.Timber;
 
 /**
@@ -87,53 +76,5 @@ public class JsonHelper {
 
         Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
         return gson.fromJson(json.toString(), type);
-    }
-
-    @Nullable
-    synchronized static JSONObject jsonReader(String jsonURL) throws IOException {
-        try {
-            Request request = new Request.Builder()
-                    .url(jsonURL)
-                    .addHeader("User-Agent", Consts.USER_AGENT)
-                    .addHeader("Data-type", "application/json")
-                    .build();
-
-            Call okHttpCall = OkHttpClientSingleton.getInstance().newCall(request);
-
-            Response okHttpResponse = okHttpCall.execute();
-
-            int responseCode = okHttpResponse.code();
-            Timber.d("HTTP Response: %s", responseCode);
-            if (404 == responseCode) {
-                return null;
-            }
-
-            ResponseBody body = okHttpResponse.body();
-            if (body != null) {
-                return new JSONObject(readInputStream(body.byteStream()));
-            } else {
-                Timber.e("JSON request body is null");
-                return null;
-            }
-        } catch (JSONException e) {
-            Timber.e(e, "JSON file not properly formatted");
-        }
-
-        return null;
-    }
-
-    private static String readInputStream(InputStream stream) throws IOException {
-        StringBuilder builder = new StringBuilder(stream.available());
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream,
-                Charset.forName("UTF-8")));
-        String line = reader.readLine();
-
-        while (line != null) {
-            builder.append(line);
-            line = reader.readLine();
-        }
-        reader.close();
-
-        return builder.toString();
     }
 }
