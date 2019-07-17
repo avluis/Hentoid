21b9501b9a9c7312082d6cdd0ef8b187ebd5ba57
Robb <35880555+RobbWatershed@users.noreply.github.com>
2019-07-17 08:30:31 +0200

    Silent download notifications

--- app/src/main/java/me/devsaki/hentoid/notification/download/DownloadNotificationChannel.java
+++ app/src/main/java/me/devsaki/hentoid/notification/download/DownloadNotificationChannel.java
@@ -9,14 +9,21 @@ import java.util.Objects;
 
 public class DownloadNotificationChannel {
 
-    static final String ID = "download";
+    private static final String ID_OLD = "download";
+    static final String ID = "downloads";
 
     public static void init(Context context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String name = "Downloads";
+            String name = "Book downloads";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(ID, name, importance);
+            channel.setSound(null, null);
+
             NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+
+            // Mandatory; it is not possible to change the sound of an existing channel after its initial creation
+            notificationManager.deleteNotificationChannel(ID_OLD);
+
             Objects.requireNonNull(notificationManager).createNotificationChannel(channel);
         }
     }
