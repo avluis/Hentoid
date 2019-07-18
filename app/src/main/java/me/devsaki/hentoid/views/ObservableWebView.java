897527a57c2c136c75fef63273199f3cd7e7aff6
Robb <35880555+RobbWatershed@users.noreply.github.com>
2019-07-18 08:34:26 +0200

    Fix crash on inflating webview on Android 5.1.1

--- app/src/main/java/me/devsaki/hentoid/views/ObservableWebView.java
+++ app/src/main/java/me/devsaki/hentoid/views/ObservableWebView.java
@@ -1,33 +1,52 @@
 package me.devsaki.hentoid.views;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.webkit.WebView;
 
 /**
  * WebView implementation with scroll listener
+ *
  * Ref: http://stackoverflow.com/questions/14752523/
  */
 public class ObservableWebView extends WebView {
     private OnScrollChangedCallback mOnScrollChangedCallback;
 
     public ObservableWebView(final Context context) {
-        super(context);
+        super(getFixedContext(context));
     }
 
     public ObservableWebView(final Context context, final AttributeSet attrs) {
-        super(context, attrs);
+        super(getFixedContext(context), attrs);
     }
 
     public ObservableWebView(final Context context, final AttributeSet attrs, final int defStyle) {
-        super(context, attrs, defStyle);
+        super(getFixedContext(context), attrs, defStyle);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public ObservableWebView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(getFixedContext(context), attrs, defStyleAttr, defStyleRes);
+    }
+
+    public ObservableWebView(Context context, AttributeSet attrs, int defStyleAttr, boolean privateBrowsing) {
+        super(getFixedContext(context), attrs, defStyleAttr, privateBrowsing);
+    }
+
+    // Fix for inflating on Android 5.1.1
+    // https://stackoverflow.com/questions/41025200/android-view-inflateexception-error-inflating-class-android-webkit-webview
+    public static Context getFixedContext(Context context) {
+        return context.createConfigurationContext(new Configuration());
     }
 
     @Override
     protected void onScrollChanged(final int l, final int t, final int oldl, final int oldt) {
         super.onScrollChanged(l, t, oldl, oldt);
-        int deltaX = l-oldl;
-        int deltaY = t-oldt;
+        int deltaX = l - oldl;
+        int deltaY = t - oldt;
         if (mOnScrollChangedCallback != null) mOnScrollChangedCallback.onScroll(deltaX, deltaY);
     }
 
