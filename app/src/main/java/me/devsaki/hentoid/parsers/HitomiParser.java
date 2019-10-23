4a0545c7313eff2b477f646dbb3a7c4f98597731
Robb <35880555+RobbWatershed@users.noreply.github.com>
2019-10-23 08:19:26 +0200

    Cleanup

--- app/src/main/java/me/devsaki/hentoid/parsers/HitomiParser.java
+++ app/src/main/java/me/devsaki/hentoid/parsers/HitomiParser.java
@@ -7,8 +7,6 @@ import androidx.annotation.NonNull;
 import com.squareup.moshi.Types;
 
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
@@ -47,41 +45,7 @@ public class HitomiParser implements ImageListParser {
         if (null == doc) throw new Exception("Document unreachable : " + pageUrl);
 
         Timber.d("Parsing: %s", pageUrl);
-        Elements imgElements = doc.select(".img-url");
 
-//        if (!imgElements.isEmpty()) return fromOldGallery(content, pageUrl, imgElements);
-//        else return fromGallery(content, pageUrl);
-
-        return fromGallery(content, pageUrl);
-    }
-
-
-    // Old layout; is still reachable but links to ultra slow hosts
-    private List<ImageFile> fromOldGallery(@NonNull Content content, @NonNull String pageUrl, @NonNull Elements imgElements) {
-        List<ImageFile> result = new ArrayList<>();
-
-        // New Hitomi image URLs starting from june 2018
-        //  If book ID is even, starts with 'aa'; else starts with 'ba'
-        int referenceId = Integer.parseInt(content.getUniqueSiteId()) % 10;
-        String imageSubdomain = subdomainFromGalleryId(referenceId);
-
-        Map<String, String> downloadParams = new HashMap<>();
-        // Add referer information to downloadParams for future image download
-        downloadParams.put(HttpHelper.HEADER_REFERER_KEY, pageUrl);
-        String downloadParamsStr = JsonHelper.serializeToJson(downloadParams, JsonHelper.MAP_STRINGS);
-
-        int order = 1;
-        for (Element element : imgElements) {
-            ImageFile img = ParseHelper.urlToImageFile("https:" + replaceSubdomainWith(element.text(), imageSubdomain), order++);
-            img.setDownloadParams(downloadParamsStr);
-            result.add(img);
-        }
-
-        return result;
-    }
-
-    // New layout as of 10/2019
-    private List<ImageFile> fromGallery(@NonNull Content content, @NonNull String pageUrl) throws IOException {
         List<ImageFile> result = new ArrayList<>();
 
         String galleryJsonUrl = "https://ltn.hitomi.la/galleries/" + content.getUniqueSiteId() + ".js";
@@ -148,14 +112,6 @@ public class HitomiParser implements ImageListParser {
 
     private String subdomainFromGalleryId(int referenceId) {
         return ((char) (HOSTNAME_PREFIX_BASE + (referenceId % NUMBER_OF_FRONTENDS))) + HOSTNAME_SUFFIX;
-    }
-
-    private String replaceSubdomainWith(String url, String newSubdomain) {
-        // Get the beginning and end of subdomain
-        int subdomainBegin = 2; // Just after '//'
-        int subdomainEnd = url.indexOf(".hitomi");
-
-        return url.substring(0, subdomainBegin) + newSubdomain + url.substring(subdomainEnd);
     }
 
     public ImageFile parseBackupUrl(String url, int order) {
