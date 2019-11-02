27db3953b9ebdc12b855e5ca8ae0f2324243497c
Robb <35880555+RobbWatershed@users.noreply.github.com>
2019-11-02 22:18:37 +0100

    Default active sites = default menu in v1.9.x

--- app/src/main/java/me/devsaki/hentoid/util/Preferences.java
+++ app/src/main/java/me/devsaki/hentoid/util/Preferences.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.preference.PreferenceManager;
+import android.text.TextUtils;
 
 import com.annimon.stream.Stream;
 
@@ -421,7 +422,8 @@ public final class Preferences {
         static final int PREF_DL_RETRIES_NUMBER = 3;
         static final int PREF_DL_RETRIES_MEM_LIMIT = 100;
         static final boolean PREF_CHECK_UPDATES_DEFAULT = true;
-        static final String ACTIVE_SITES = Site.HITOMI.getCode() + "," + Site.NHENTAI.getCode() + "," + Site.HENTAICAFE.getCode() + "," + Site.TSUMINO.getCode() + "," + Site.EHENTAI.getCode();
+        // Default menu in v1.9.x
+        static final String ACTIVE_SITES = TextUtils.join(",", new Site[]{Site.NHENTAI, Site.HENTAICAFE, Site.HITOMI, Site.ASMHENTAI, Site.TSUMINO, Site.PURURIN, Site.EHENTAI, Site.FAKKU2, Site.NEXUS, Site.MUSES, Site.DOUJINS});
     }
 
     // IMPORTANT : Any value change must be mirrored in res/values/array_preferences.xml
