132988a2e9dced65c28ecd3516645b6045304bd8
Robb <35880555+RobbWatershed@users.noreply.github.com>
2020-02-09 15:49:19 +0100

    Implement timer for when "lock on app leave" is on (#422)

--- app/src/main/java/me/devsaki/hentoid/fragments/pin/UnlockPinDialogFragment.java
+++ app/src/main/java/me/devsaki/hentoid/fragments/pin/UnlockPinDialogFragment.java
@@ -1,18 +1,14 @@
 package me.devsaki.hentoid.fragments.pin;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import me.devsaki.hentoid.R;
-import me.devsaki.hentoid.fragments.viewer.BrowseModeDialogFragment;
 import me.devsaki.hentoid.util.Preferences;
 
 public final class UnlockPinDialogFragment extends PinDialogFragment {
