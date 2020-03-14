960473c14b05fb236f280da504da3f098c703930
Robb <35880555+RobbWatershed@users.noreply.github.com>
2020-03-14 10:02:26 +0100

    Make Sonar happy

--- app/src/main/java/me/devsaki/hentoid/activities/bundles/SearchActivityBundle.java
+++ app/src/main/java/me/devsaki/hentoid/activities/bundles/SearchActivityBundle.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -49,11 +50,16 @@ public class SearchActivityBundle {
             Uri.Builder searchUri = new Uri.Builder()
                     .scheme("search")
                     .authority("hentoid");
-            for (AttributeType attrType : metadataMap.keySet()) {
-                List<Attribute> attrs = metadataMap.get(attrType);
-                for (Attribute attr : attrs)
-                    searchUri.appendQueryParameter(attrType.name(), attr.getId() + ";" + attr.getName());
+
+
+            for (Map.Entry<AttributeType, List<Attribute>> entry : metadataMap.entrySet()) {
+                AttributeType attrType = entry.getKey();
+                List<Attribute> attrs = entry.getValue();
+                if (attrs != null)
+                    for (Attribute attr : attrs)
+                        searchUri.appendQueryParameter(attrType.name(), attr.getId() + ";" + attr.getName());
             }
+
             return searchUri.build();
         }
 
