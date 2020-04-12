c16d955ced09ca376ba999bfccc23aa14e31c117
Robb <35880555+RobbWatershed@users.noreply.github.com>
2020-04-12 16:39:05 +0200

    Android 10 library migration task

--- app/src/main/java/me/devsaki/hentoid/activities/SplashActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/SplashActivity.java
@@ -1,7 +1,9 @@
 package me.devsaki.hentoid.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -9,7 +11,11 @@ import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.lmntrx.android.library.livin.missme.ProgressDialog;
+
 import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.io.File;
 
@@ -18,12 +24,18 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
 import me.devsaki.hentoid.BuildConfig;
+import me.devsaki.hentoid.HentoidApp;
 import me.devsaki.hentoid.R;
+import me.devsaki.hentoid.activities.bundles.ImportActivityBundle;
 import me.devsaki.hentoid.database.ObjectBoxDB;
 import me.devsaki.hentoid.events.AppUpdatedEvent;
+import me.devsaki.hentoid.events.ImportEvent;
+import me.devsaki.hentoid.notification.import_.ImportNotificationChannel;
+import me.devsaki.hentoid.services.ImportService;
 import me.devsaki.hentoid.util.FileHelper;
 import me.devsaki.hentoid.util.Helper;
 import me.devsaki.hentoid.util.Preferences;
+import me.devsaki.hentoid.util.ThemeHelper;
 import me.devsaki.hentoid.views.NestedScrollWebView;
 import timber.log.Timber;
 
@@ -34,7 +46,9 @@ import timber.log.Timber;
  */
 public class SplashActivity extends AppCompatActivity {
 
+    private ProgressDialog progressDialog;
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private ObjectBoxDB db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +56,7 @@ public class SplashActivity extends AppCompatActivity {
 
         Timber.d("Splash / Init");
 
+        EventBus.getDefault().register(this);
         // Pre-processing on app update
         if (Preferences.getLastKnownAppVersionCode() < BuildConfig.VERSION_CODE) {
             Timber.d("Splash / Update detected");
@@ -57,21 +72,77 @@ public class SplashActivity extends AppCompatActivity {
         if (Preferences.isFirstRun()) {
             goToActivity(new Intent(this, IntroActivity.class));
         } else if (hasToMigrateAndroid10()) {
-            // TODO smth
+            runAndroid10Migration();
         } else {
             goToLibraryActivity();
         }
     }
 
+    /**
+     * Test if the ImageFiles stored in the DB have their URI's filled
+     * If not, it indicates the collection has not been updated to fit the Android 10 I/O update
+     *
+     * @return True if a migration has to happen; false if not
+     */
     private boolean hasToMigrateAndroid10() {
-        ObjectBoxDB db = ObjectBoxDB.getInstance(this);
+        db = ObjectBoxDB.getInstance(this);
         long imagesKO = db.countDownloadedImagesWithoutUri();
         Timber.i(">> count10 %s", imagesKO);
         return imagesKO > 0;
     }
 
+    private void runAndroid10Migration() {
+        // Prior Library found, drop and recreate db
+        cleanUpDB();
+        // Send results to scan
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setIndeterminate(false);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setMax(0);
+        progressDialog.setColor(ThemeHelper.getColor(this, R.color.secondary_light));
+        progressDialog.setTextColor(R.color.white_opacity_87);
+        progressDialog.setMessage(this.getString(R.string.updating_please_wait));
+        progressDialog.show();
+
+        ImportNotificationChannel.init(this);
+        Intent intent = ImportService.makeIntent(this);
+
+        ImportActivityBundle.Builder builder = new ImportActivityBundle.Builder();
+        builder.setRefreshRename(false);
+        builder.setRefreshCleanAbsent(false);
+        builder.setRefreshCleanNoImages(false);
+        builder.setRefreshCleanUnreadable(false);
+        intent.putExtras(builder.getBundle());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent);
+        } else {
+            startService(intent);
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onImportEvent(ImportEvent event) {
+        if (ImportEvent.EV_PROGRESS == event.eventType) {
+            progressDialog.setMax(event.booksTotal);
+            progressDialog.setProgress(event.booksOK + event.booksKO);
+        } else if (ImportEvent.EV_COMPLETE == event.eventType) {
+            if (progressDialog != null) progressDialog.dismiss();
+            goToLibraryActivity();
+        }
+    }
+
+    private void cleanUpDB() {
+        Timber.d("Cleaning up DB.");
+        Context context = HentoidApp.getInstance();
+        ObjectBoxDB db = ObjectBoxDB.getInstance(context);
+        db.deleteAllBooks();
+    }
+
     @Override
     protected void onDestroy() {
+        EventBus.getDefault().unregister(this);
         compositeDisposable.clear();
 
         super.onDestroy();
