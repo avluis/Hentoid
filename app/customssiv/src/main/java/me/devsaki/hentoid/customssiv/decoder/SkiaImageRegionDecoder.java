112a48bc2be2647bcab1092eb18bce287a188e30
Robb <35880555+RobbWatershed@users.noreply.github.com>
2020-04-26 10:31:47 +0200

    Remove kitkat specifics

--- app/customssiv/src/main/java/me/devsaki/hentoid/customssiv/decoder/SkiaImageRegionDecoder.java
+++ app/customssiv/src/main/java/me/devsaki/hentoid/customssiv/decoder/SkiaImageRegionDecoder.java
@@ -162,10 +162,6 @@ public class SkiaImageRegionDecoder implements ImageRegionDecoder {
      * use the write lock to enforce single threaded decoding.
      */
     private Lock getDecodeLock() {
-        if (Build.VERSION.SDK_INT < 21) {
-            return decoderLock.writeLock();
-        } else {
-            return decoderLock.readLock();
-        }
+        return decoderLock.readLock();
     }
 }
