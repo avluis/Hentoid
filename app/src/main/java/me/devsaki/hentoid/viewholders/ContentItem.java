d47c4f836b2eb9af30c604a6842353ef1b73575c
Robb <35880555+RobbWatershed@users.noreply.github.com>
2020-06-14 17:41:21 +0200

    Display estimated download size with a more explicit label

--- app/src/main/java/me/devsaki/hentoid/viewholders/ContentItem.java
+++ app/src/main/java/me/devsaki/hentoid/viewholders/ContentItem.java
@@ -487,7 +487,7 @@ public class ContentItem extends AbstractItem<ContentItem.ContentViewHolder> imp
                         String pagesText = tvPages.getText().toString();
                         int separator = pagesText.indexOf(";");
                         if (separator > -1) pagesText = pagesText.substring(0, separator);
-                        pagesText = pagesText + String.format(Locale.US, "; %.1f MB (est.)", content.getBookSizeEstimate() / (1024 * 1024));
+                        pagesText = pagesText + String.format(Locale.US, "; estimated %.1f MB", content.getBookSizeEstimate() / (1024 * 1024));
                         tvPages.setText(pagesText);
                     }
                 } else {
