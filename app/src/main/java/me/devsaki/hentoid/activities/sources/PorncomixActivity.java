7c5a26e5251a7911439255bdb149d39a3eb01bae
Robb <35880555+RobbWatershed@users.noreply.github.com>
2020-09-30 21:52:25 +0200

    Align porncomix parser to new porncomix URL schemes and domains

--- app/src/main/java/me/devsaki/hentoid/activities/sources/PorncomixActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/PorncomixActivity.java
@@ -4,13 +4,14 @@ import me.devsaki.hentoid.enums.Site;
 
 public class PorncomixActivity extends BaseWebActivity {
 
-    private static final String[] DOMAIN_FILTER = {"www.porncomixonline.net", "porncomicszone.net", "porncomixinfo.com", "bestporncomix.com"};
+    private static final String[] DOMAIN_FILTER = {"www.porncomixonline.net", "porncomicszone.net", "porncomixinfo.com", "porncomixinfo.net", "bestporncomix.com"};
     private static final String[] GALLERY_FILTER = {
-            "//www.porncomixonline.net/manga/[A-Za-z0-9\\-]+/[A-Za-z0-9\\-]+$",
-            "//www.porncomixonline.net/[A-Za-z0-9\\-]+/$",
-            "//porncomicszone.net/[0-9]+/[A-Za-z0-9\\-]+/[0-9]+/$",
-            "//porncomixinfo.com/manga-comics/[A-Za-z0-9\\-]+/[A-Za-z0-9\\-]+/$",
-            "//bestporncomix.com/gallery/[A-Za-z0-9\\-]+/$"
+            "//www.porncomixonline.net/(?!m-comic)([\\w\\-]+)/[\\w\\-]+/$",
+            "//www.porncomixonline.net/m-comic/[\\w\\-]+/[\\w\\-]+$",
+            "//porncomicszone.net/[0-9]+/[\\w\\-]+/[0-9]+/$",
+            "//porncomixinfo.(com|net)/manga-comics/[\\w\\-]+/[\\w\\-]+/$",
+            "//porncomixinfo.(com|net)/chapter/[\\w\\-]+/[\\w\\-]+/$",
+            "//bestporncomix.com/gallery/[\\w\\-]+/$"
     };
     private static final String[] DIRTY_ELEMENTS = {"iframe[name^='spot']"};
 
