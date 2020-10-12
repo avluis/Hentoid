ab3a12dc2d8fa39634fc3e6d757e6e216bf88262
Robb <35880555+RobbWatershed@users.noreply.github.com>
2020-10-12 21:59:48 +0200

    Update to Gradle plugin 4.1.0

--- app/src/main/java/me/devsaki/hentoid/fragments/about/ChangelogFragment.kt
+++ app/src/main/java/me/devsaki/hentoid/fragments/about/ChangelogFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.observe
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.adapters.ItemAdapter
 import kotlinx.android.synthetic.main.fragment_changelog.*
