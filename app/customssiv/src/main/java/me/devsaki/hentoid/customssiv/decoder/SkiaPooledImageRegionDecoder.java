533f80cc5be76de037296567b4148fb152d53067
Robb <35880555+RobbWatershed@users.noreply.github.com>
2020-10-16 20:42:42 +0200

    Factorize code to make Sonar happy

--- app/customssiv/src/main/java/me/devsaki/hentoid/customssiv/decoder/SkiaPooledImageRegionDecoder.java
+++ app/customssiv/src/main/java/me/devsaki/hentoid/customssiv/decoder/SkiaPooledImageRegionDecoder.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapRegionDecoder;
@@ -15,7 +14,6 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
-import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.annotation.Keep;
@@ -25,7 +23,6 @@ import androidx.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
@@ -162,28 +159,7 @@ public class SkiaPooledImageRegionDecoder implements ImageRegionDecoder {
         BitmapRegionDecoder decoder;
         long localFileLength = Long.MAX_VALUE;
         if (uriString.startsWith(RESOURCE_PREFIX)) {
-            Resources res;
-            String packageName = uri.getAuthority();
-            if (context.getPackageName().equals(packageName)) {
-                res = context.getResources();
-            } else {
-                PackageManager pm = context.getPackageManager();
-                res = pm.getResourcesForApplication(packageName);
-            }
-
-            int id = 0;
-            List<String> segments = uri.getPathSegments();
-            int size = segments.size();
-            if (size == 2 && segments.get(0).equals("drawable")) {
-                String resName = segments.get(1);
-                id = res.getIdentifier(resName, "drawable", packageName);
-            } else if (size == 1 && TextUtils.isDigitsOnly(segments.get(0))) {
-                try {
-                    id = Integer.parseInt(segments.get(0));
-                } catch (NumberFormatException ignored) {
-                    // Ignored exception
-                }
-            }
+            int id = SkiaDecoderHelper.getResourceId(context, uri);
             try (AssetFileDescriptor descriptor = context.getResources().openRawResourceFd(id)) {
                 localFileLength = descriptor.getLength();
             } catch (Exception e) {
