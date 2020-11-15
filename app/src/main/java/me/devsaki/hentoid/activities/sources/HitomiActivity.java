36e7ed921c7e48e4751686f6e411550903e45eb7
Robb <35880555+RobbWatershed@users.noreply.github.com>
2020-11-15 10:04:09 +0100

    Improve regexp

--- app/src/main/java/me/devsaki/hentoid/activities/sources/HitomiActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/HitomiActivity.java
@@ -25,7 +25,7 @@ public class HitomiActivity extends BaseWebActivity {
 
     private static final String DOMAIN_FILTER = "hitomi.la";
     private static final String[] GALLERY_FILTER = {"//hitomi.la/[\\w%\\-]+/[^/]+-[0-9]{2,}.html$"};
-    private static final String[] RESULTS_FILTER = {"//hitomi.la[/]*$", "//hitomi.la[/]*\\?", "//hitomi.la/search.html", "//hitomi.la/index-[\\w%\\-\\.\\?]+", "//hitomi.la/(series|artist|tag|character)/[\\w%\\-\\.\\?]+"};
+    private static final String[] RESULTS_FILTER = {"//hitomi.la[/]{0,1}$", "//hitomi.la[/]{0,1}\\?", "//hitomi.la/search.html", "//hitomi.la/index-[\\w%\\-\\.\\?]+", "//hitomi.la/(series|artist|tag|character)/[\\w%\\-\\.\\?]+"};
     private static final String[] BLOCKED_CONTENT = {"hitomi-horizontal.js", "hitomi-vertical.js", "invoke.js", "ion.sound"};
     private static final String[] JS_WHITELIST = {"galleries/[\\w%\\-]+.js$", "jquery", "filesaver", "common", "date", "download", "gallery", "jquery", "cookie", "jszip", "limitlists", "moment-with-locales", "moveimage", "pagination", "search", "searchlib", "yall", "reader", "decode_webp", "bootstrap"};
     private static final String[] BLOCKED_JS_CONTENTS = {"exoloader", "popunder"};
