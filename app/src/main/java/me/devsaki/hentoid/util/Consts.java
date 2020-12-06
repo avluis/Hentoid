f468a32e11543abde9a2e6ba9cffc41cb38e6550
Robb <35880555+RobbWatershed@users.noreply.github.com>
2020-12-06 20:51:35 +0100

    Cleanup

--- app/src/main/java/me/devsaki/hentoid/util/Consts.java
+++ app/src/main/java/me/devsaki/hentoid/util/Consts.java
@@ -10,8 +10,6 @@ public abstract class Consts {
         throw new IllegalStateException("Utility class");
     }
 
-    public static final String DATABASE_NAME = "hentoid.db";
-
     public static final String DEFAULT_LOCAL_DIRECTORY_OLD = "Hentoid";
     public static final String DEFAULT_LOCAL_DIRECTORY = ".Hentoid";
 
