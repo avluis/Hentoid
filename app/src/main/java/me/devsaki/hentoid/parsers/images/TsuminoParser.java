b73c9169ead8d3cc652bad0356cbeb94d637194a
Robb <35880555+RobbWatershed@users.noreply.github.com>
2020-12-15 21:45:26 +0100

    Tsumino parser now uses proper cookies and user agent

--- app/src/main/java/me/devsaki/hentoid/parsers/images/TsuminoParser.java
+++ app/src/main/java/me/devsaki/hentoid/parsers/images/TsuminoParser.java
@@ -1,17 +1,25 @@
 package me.devsaki.hentoid.parsers.images;
 
+import android.util.Pair;
+
 import androidx.annotation.NonNull;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import me.devsaki.hentoid.database.domains.Content;
+import me.devsaki.hentoid.enums.Site;
+import me.devsaki.hentoid.util.JsonHelper;
 import me.devsaki.hentoid.util.exception.CaptchaException;
+import me.devsaki.hentoid.util.network.HttpHelper;
+import timber.log.Timber;
 
 import static me.devsaki.hentoid.util.network.HttpHelper.getOnlineDocument;
 
@@ -23,8 +31,29 @@ public class TsuminoParser extends BaseParser {
 
     @Override
     protected List<String> parseImages(@NonNull Content content) throws Exception {
+        List<String> result = new ArrayList<>();
+
+        String downloadParamsStr = content.getDownloadParams();
+        if (null == downloadParamsStr || downloadParamsStr.isEmpty()) {
+            Timber.e("Download parameters not set");
+            return result;
+        }
+
+        Map<String, String> downloadParams;
+        try {
+            downloadParams = JsonHelper.jsonToObject(downloadParamsStr, JsonHelper.MAP_STRINGS);
+        } catch (IOException e) {
+            Timber.e(e);
+            return result;
+        }
+
+        List<Pair<String, String>> headers = new ArrayList<>();
+        String cookieStr = downloadParams.get(HttpHelper.HEADER_COOKIE_KEY);
+        if (null != cookieStr)
+            headers.add(new Pair<>(HttpHelper.HEADER_COOKIE_KEY, cookieStr));
+
         // Fetch the reader page
-        Document doc = getOnlineDocument(content.getReaderUrl());
+        Document doc = getOnlineDocument(content.getReaderUrl(), headers, Site.TSUMINO.useHentoidAgent());
         if (null != doc) {
             Elements captcha = doc.select(".g-recaptcha");
             if (captcha != null && !captcha.isEmpty())
