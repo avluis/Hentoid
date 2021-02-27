656a2de3943a53ef171a3462c3689ba17dde10b7
Robb <35880555+RobbWatershed@users.noreply.github.com>
2021-02-27 18:57:47 +0100

    Put check at the correct place

--- app/customssiv/src/main/java/me/devsaki/hentoid/customssiv/ResizeBitmapHelper.java
+++ app/customssiv/src/main/java/me/devsaki/hentoid/customssiv/ResizeBitmapHelper.java
@@ -23,6 +23,7 @@ class ResizeBitmapHelper {
 
 
     static ImmutablePair<Bitmap, Float> resizeBitmap(final RenderScript rs, @NonNull final Bitmap src, float targetScale) {
+        Helper.mustNotRunOnUiThread();
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || null == rs) { // Because Renderscript is super unstable on Android 5 (see https://issuetracker.google.com/issues/119582492; reported by users)
             ImmutablePair<Integer, Float> resizeParams = computeResizeParams(targetScale);
             Timber.d(">> resizing successively to scale %s", resizeParams.right);
