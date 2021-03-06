81fff4d981084c5cae128159e938fa53f8866fea
Robb <35880555+RobbWatershed@users.noreply.github.com>
2021-03-06 19:15:02 +0100

    Remove support for Hentai Cafe

--- app/src/main/java/me/devsaki/hentoid/util/Preferences.java
+++ app/src/main/java/me/devsaki/hentoid/util/Preferences.java
@@ -790,7 +790,7 @@ public final class Preferences {
         static final boolean DL_HITOMI_WEBP = true;
         static final boolean CHECK_UPDATES = true;
         // Default menu in v1.9.x
-        static final Site[] DEFAULT_SITES = new Site[]{Site.NHENTAI, Site.HENTAICAFE, Site.HITOMI, Site.ASMHENTAI, Site.TSUMINO, Site.PURURIN, Site.EHENTAI, Site.FAKKU2, Site.NEXUS, Site.MUSES, Site.DOUJINS};
+        static final Site[] DEFAULT_SITES = new Site[]{Site.NHENTAI, Site.HITOMI, Site.ASMHENTAI, Site.TSUMINO, Site.PURURIN, Site.EHENTAI, Site.FAKKU2, Site.NEXUS, Site.MUSES, Site.DOUJINS};
         static final String ACTIVE_SITES = TextUtils.join(",", Stream.of(DEFAULT_SITES).map(Site::getCode).toList());
         static final boolean LOCK_ON_APP_RESTORE = false;
         static final int LOCK_TIMER = Constant.LOCK_TIMER_30S;
