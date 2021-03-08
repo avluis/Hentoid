dd50b39df96951da41e9910b41dd7c8a464eb37f
Robb <35880555+RobbWatershed@users.noreply.github.com>
2021-03-08 20:27:21 +0100

    Restrict cover-based update detection to Manwha and Toonily

--- app/src/main/java/me/devsaki/hentoid/enums/Site.java
+++ app/src/main/java/me/devsaki/hentoid/enums/Site.java
@@ -16,27 +16,27 @@ public enum Site {
     // NOTE : to maintain compatiblity with saved JSON files and prefs, do _not_ edit either existing names or codes
     FAKKU(0, "Fakku", "https://www.fakku.net", R.drawable.ic_menu_fakku), // Legacy support for old fakku archives
     PURURIN(1, "Pururin", "https://pururin.io", R.drawable.ic_menu_pururin),
-    HITOMI(2, "hitomi", "https://hitomi.la", R.drawable.ic_menu_hitomi, false, false, false, false), // Hitomi needs a desktop agent to properly display gallery images on some devices
-    NHENTAI(3, "nhentai", "https://nhentai.net", R.drawable.ic_menu_nhentai, true, true, false, false),
-    TSUMINO(4, "tsumino", "https://www.tsumino.com", R.drawable.ic_menu_tsumino, true, true, false, false),
-    HENTAICAFE(5, "hentaicafe", "https://hentai.cafe", R.drawable.ic_menu_hentaicafe, true, true, false, false),
-    ASMHENTAI(6, "asmhentai", "https://asmhentai.com", R.drawable.ic_menu_asmhentai, true, true, false, false),
-    ASMHENTAI_COMICS(7, "asmhentai comics", "https://comics.asmhentai.com", R.drawable.ic_menu_asmcomics, true, true, false, false),
-    EHENTAI(8, "e-hentai", "https://e-hentai.org", R.drawable.ic_menu_ehentai, true, true, false, true),
-    FAKKU2(9, "Fakku", "https://www.fakku.net", R.drawable.ic_menu_fakku, true, false, true, false),
+    HITOMI(2, "hitomi", "https://hitomi.la", R.drawable.ic_menu_hitomi, false, false, false, false, false), // Hitomi needs a desktop agent to properly display gallery images on some devices
+    NHENTAI(3, "nhentai", "https://nhentai.net", R.drawable.ic_menu_nhentai, true, true, false, false, false),
+    TSUMINO(4, "tsumino", "https://www.tsumino.com", R.drawable.ic_menu_tsumino, true, true, false, false, false),
+    HENTAICAFE(5, "hentaicafe", "https://hentai.cafe", R.drawable.ic_menu_hentaicafe, true, true, false, false, false),
+    ASMHENTAI(6, "asmhentai", "https://asmhentai.com", R.drawable.ic_menu_asmhentai, true, true, false, false, false),
+    ASMHENTAI_COMICS(7, "asmhentai comics", "https://comics.asmhentai.com", R.drawable.ic_menu_asmcomics, true, true, false, false, false),
+    EHENTAI(8, "e-hentai", "https://e-hentai.org", R.drawable.ic_menu_ehentai, true, true, false, true, false),
+    FAKKU2(9, "Fakku", "https://www.fakku.net", R.drawable.ic_menu_fakku, true, false, true, false, false),
     NEXUS(10, "Hentai Nexus", "https://hentainexus.com", R.drawable.ic_menu_nexus),
     MUSES(11, "8Muses", "https://www.8muses.com", R.drawable.ic_menu_8muses),
     DOUJINS(12, "doujins.com", "https://doujins.com/", R.drawable.ic_menu_doujins),
     LUSCIOUS(13, "luscious.net", "https://members.luscious.net/manga/", R.drawable.ic_menu_luscious),
-    EXHENTAI(14, "exhentai", "https://exhentai.org", R.drawable.ic_menu_exhentai, true, false, false, true),
+    EXHENTAI(14, "exhentai", "https://exhentai.org", R.drawable.ic_menu_exhentai, true, false, false, true, false),
     PORNCOMIX(15, "porncomixonline", "https://www.porncomixonline.net/", R.drawable.ic_menu_porncomix),
     HBROWSE(16, "Hbrowse", "https://www.hbrowse.com/", R.drawable.ic_menu_hbrowse),
     HENTAI2READ(17, "Hentai2Read", "https://hentai2read.com/", R.drawable.ic_menu_hentai2read),
     HENTAIFOX(18, "Hentaifox", "https://hentaifox.com", R.drawable.ic_menu_hentaifox),
     MRM(19, "MyReadingManga", "https://myreadingmanga.info/", R.drawable.ic_menu_mrm),
-    MANHWA(20, "ManwhaHentai", "https://manhwahentai.me/", R.drawable.ic_menu_manhwa),
+    MANHWA(20, "ManwhaHentai", "https://manhwahentai.me/", R.drawable.ic_menu_manhwa, true, false, false, false, true), // Cover-based page updates
     IMHENTAI(21, "Imhentai", "https://imhentai.xxx", R.drawable.ic_menu_imhentai),
-    TOONILY(22, "Toonily", "https://toonily.com/", R.drawable.ic_menu_toonily),
+    TOONILY(22, "Toonily", "https://toonily.com/", R.drawable.ic_menu_toonily, true, false, false, false, true), // Cover-based page updates
     NONE(98, "none", "", R.drawable.ic_external_library), // External library; fallback site
     PANDA(99, "panda", "https://www.mangapanda.com", R.drawable.ic_menu_panda); // Safe-for-work/wife/gf option; not used anymore and kept here for retrocompatibility
 
@@ -58,6 +58,7 @@ public enum Site {
     private final boolean useHentoidAgent;
     private final boolean hasImageProcessing;
     private final boolean hasBackupURLs;
+    private final boolean hasCoverBasedPageUpdates;
 
     Site(int code,
          String description,
@@ -66,7 +67,8 @@ public enum Site {
          boolean useMobileAgent,
          boolean useHentoidAgent,
          boolean hasImageProcessing,
-         boolean hasBackupURLs) {
+         boolean hasBackupURLs,
+         boolean hasCoverBasedPageUpdates) {
         this.code = code;
         this.description = description;
         this.url = url;
@@ -75,6 +77,7 @@ public enum Site {
         this.useHentoidAgent = useHentoidAgent;
         this.hasImageProcessing = hasImageProcessing;
         this.hasBackupURLs = hasBackupURLs;
+        this.hasCoverBasedPageUpdates = hasCoverBasedPageUpdates;
     }
 
     Site(int code,
@@ -89,6 +92,7 @@ public enum Site {
         this.useHentoidAgent = false;
         this.hasImageProcessing = false;
         this.hasBackupURLs = false;
+        this.hasCoverBasedPageUpdates = false;
     }
 
     public static Site searchByCode(long code) {
@@ -151,6 +155,10 @@ public enum Site {
 
     public boolean hasBackupURLs() {
         return hasBackupURLs;
+    }
+
+    public boolean hasCoverBasedPageUpdates() {
+        return hasCoverBasedPageUpdates;
     }
 
     public boolean isVisible() {
