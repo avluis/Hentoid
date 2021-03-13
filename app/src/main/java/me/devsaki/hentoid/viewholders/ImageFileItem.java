8bb6d99f02e2670d479c1ba253b99ee0969829fb
Robb <35880555+RobbWatershed@users.noreply.github.com>
2021-03-13 16:44:05 +0100

    Make Sonar happy

--- app/src/main/java/me/devsaki/hentoid/viewholders/ImageFileItem.java
+++ app/src/main/java/me/devsaki/hentoid/viewholders/ImageFileItem.java
@@ -38,7 +38,7 @@ public class ImageFileItem extends AbstractItem<ImageFileItem.ImageViewHolder> {
 
     // Return a copy, not the original instance that has to remain in synch with its visual representation
     public ImageFile getImage() {
-        return image.clone();
+        return new ImageFile(image);
     }
 
     public void setCurrent(boolean current) {
