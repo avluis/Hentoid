5fadda073b7b314c0bf1fe833e047e5a83e7b1a5
Robb <35880555+RobbWatershed@users.noreply.github.com>
2021-04-12 13:43:43 +0200

    Tooltips for micro-menus

--- app/src/main/java/me/devsaki/hentoid/fragments/viewer/ViewerPagerFragment.java
+++ app/src/main/java/me/devsaki/hentoid/fragments/viewer/ViewerPagerFragment.java
@@ -375,8 +375,12 @@ public class ViewerPagerFragment extends Fragment implements ViewerBrowseModeDia
 
         // Information micro menu
         binding.controlsOverlay.informationMicroMenu.setSubmarineItemClickListener((p, i) -> onInfoMicroMenuClick(p));
-        binding.controlsOverlay.informationMicroMenu.addSubmarineItem(new SubmarineItem(ContextCompat.getDrawable(requireContext(), R.drawable.ic_book)));
-        binding.controlsOverlay.informationMicroMenu.addSubmarineItem(new SubmarineItem(ContextCompat.getDrawable(requireContext(), R.drawable.ic_page)));
+        binding.controlsOverlay.informationMicroMenu.addSubmarineItem(
+                new SubmarineItem(ContextCompat.getDrawable(requireContext(), R.drawable.ic_book), null, getResources().getString(R.string.book_details))
+        );
+        binding.controlsOverlay.informationMicroMenu.addSubmarineItem(
+                new SubmarineItem(ContextCompat.getDrawable(requireContext(), R.drawable.ic_page), null, getResources().getString(R.string.page_details))
+        );
         binding.controlsOverlay.viewerInfoBtn.setOnClickListener(v -> {
             binding.controlsOverlay.favouriteMicroMenu.dips();
             binding.controlsOverlay.informationMicroMenu.floats();
@@ -457,8 +461,19 @@ public class ViewerPagerFragment extends Fragment implements ViewerBrowseModeDia
 
         SubmarineView favMenu = binding.controlsOverlay.favouriteMicroMenu;
         favMenu.clearAllSubmarineItems();
-        favMenu.addSubmarineItem(new SubmarineItem(ContextCompat.getDrawable(requireContext(), isContentFavourite ? R.drawable.ic_book_fav : R.drawable.ic_book)));
-        favMenu.addSubmarineItem(new SubmarineItem(ContextCompat.getDrawable(requireContext(), isPageFavourite ? R.drawable.ic_page_fav : R.drawable.ic_page)));
+        favMenu.addSubmarineItem(
+                new SubmarineItem(
+                        ContextCompat.getDrawable(requireContext(), isContentFavourite ? R.drawable.ic_book_fav : R.drawable.ic_book),
+                        null,
+                        getResources().getString(R.string.book_favourite_toggle)
+                )
+        );
+        favMenu.addSubmarineItem(
+                new SubmarineItem(ContextCompat.getDrawable(requireContext(), isPageFavourite ? R.drawable.ic_page_fav : R.drawable.ic_page),
+                        null,
+                        getResources().getString(R.string.page_favourite_toggle)
+                )
+        );
         favMenu.floats();
     }
 
