d0a8bce6cfaa8be5f40a90fc63807fc942869d85
Robb <35880555+RobbWatershed@users.noreply.github.com>
2021-05-21 22:18:49 +0200

    Fix startup worker not running all tasks

--- app/src/main/java/me/devsaki/hentoid/workers/StartupWorker.java
+++ app/src/main/java/me/devsaki/hentoid/workers/StartupWorker.java
@@ -9,10 +9,7 @@ import androidx.work.WorkerParameters;
 import java.util.List;
 
 import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
 import me.devsaki.hentoid.R;
 import me.devsaki.hentoid.core.AppStartup;
 import me.devsaki.hentoid.database.DatabaseMaintenance;
@@ -44,51 +41,25 @@ public class StartupWorker extends BaseWorker {
 
     @Override
     void onInterrupt() {
-        // Nothing; won't happen
+        if (launchDisposable != null) launchDisposable.dispose();
     }
 
     @Override
     void onClear() {
         launchTasks.clear();
+        if (launchDisposable != null) launchDisposable.dispose();
     }
 
     @Override
     void getToWork(@NonNull Data input) {
         launchTasks = AppStartup.getPostLaunchTasks(getApplicationContext());
         launchTasks.addAll(DatabaseMaintenance.getPostLaunchCleanupTasks(getApplicationContext()));
-        doRunTask(0,
-                v -> notificationManager.notify(new StartupProgressNotification(Math.round(v * 100), 100)),
-                v -> {
-                },
-                () -> notificationManager.notify(new StartupCompleteNotification()));
-    }
 
-    private void doRunTask(
-            int taskIndex,
-            @NonNull Consumer<Float> onMainProgress,
-            @NonNull Consumer<Float> onSecondaryProgress,
-            @NonNull Runnable onComplete
-    ) {
-        if (launchDisposable != null) launchDisposable.dispose();
-        try {
-            onMainProgress.accept(taskIndex * 1f / launchTasks.size());
-        } catch (Exception e) {
-            Timber.w(e);
-        }
-        // Continue executing launch tasks
-        if (taskIndex < launchTasks.size()) {
-            Timber.i("Post-launch task %s/%s", taskIndex + 1, launchTasks.size());
-            launchDisposable = launchTasks.get(taskIndex)
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(
-                            onSecondaryProgress,
-                            Timber::e,
-                            () -> doRunTask(taskIndex + 1, onMainProgress, onSecondaryProgress, onComplete)
-                    );
-        } else {
-            if (launchDisposable != null) launchDisposable.dispose();
-            onComplete.run();
-        }
+        launchDisposable = Observable.concat(launchTasks)
+                .subscribe(
+                        v -> notificationManager.notify(new StartupProgressNotification(Math.round(v * 100), 100)),
+                        Timber::e,
+                        () -> notificationManager.notify(new StartupCompleteNotification())
+                );
     }
 }
