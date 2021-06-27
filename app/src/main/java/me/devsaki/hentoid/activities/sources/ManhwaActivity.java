d67e33ebaaa63d741ab94ab0dc66dd2c9f11a862
Robb <35880555+RobbWatershed@users.noreply.github.com>
2021-06-27 21:49:23 +0200

    Fix ads & downloads URLs

--- app/src/main/java/me/devsaki/hentoid/activities/sources/ManhwaActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/ManhwaActivity.java
@@ -7,7 +7,7 @@ public class ManhwaActivity extends BaseWebActivity {
     private static final String DOMAIN_FILTER = "manhwahentai.me";
     private static final String[] GALLERY_FILTER = {"//manhwahentai.me/[\\w\\-]+/[\\w\\-]{3,}/$"};
     private static final String[] DIRTY_ELEMENTS = {".c-ads"};
-    private static final String[] JS_CONTENT_BLACKLIST = {"'iframe'"};
+    private static final String[] JS_CONTENT_BLACKLIST = {"'iframe'","'adsdomain'","'closead'"};
     private static final String[] BLOCKED_CONTENT = {".cloudfront.net"};
 
 
