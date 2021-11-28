fa54fc3930ddc4144881a2e34cdfea0e5ae1b2a6
Robb <35880555+robbwatershed@users.noreply.github.com>
2021-11-28 16:43:18 +0100

    Use Glide over OkHttp

--- .proguard/proguard-glide.pro
+++ .proguard/proguard-glide.pro
@@ -1,8 +1,9 @@
 # Glide specific rules #
 # https://github.com/bumptech/glide
 
--keep public class * implements com.bumptech.glide.module.GlideModule
 -keep public class * extends com.bumptech.glide.module.AppGlideModule
+-keep class com.bumptech.glide.GeneratedAppGlideModuleImpl
+
 -keep public enum com.bumptech.glide.load.ImageHeaderParser$** {
   **[] $VALUES;
   public *;
---
+++ app/src/main/java/me/devsaki/hentoid/core/CustomGlideModule.java
@@ -0,0 +1,30 @@
+package me.devsaki.hentoid.core;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.module.AppGlideModule;
+
+import java.io.InputStream;
+
+import me.devsaki.hentoid.util.network.OkHttpClientSingleton;
+import okhttp3.OkHttpClient;
+
+@GlideModule
+public final class CustomGlideModule extends AppGlideModule {
+
+    @Override
+    public void registerComponents(@NonNull Context context, Glide glide, @NonNull Registry registry) {
+        OkHttpClient client = OkHttpClientSingleton.getInstance();
+
+        OkHttpUrlLoader.Factory factory = new OkHttpUrlLoader.Factory(client);
+
+        glide.getRegistry().replace(GlideUrl.class, InputStream.class, factory);
+    }
+}
