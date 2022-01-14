8d21bd0a339335ae683f1f4f95839f64a16cf94c
Robb <35880555+robbwatershed@users.noreply.github.com>
2022-01-14 08:26:11 +0100

    Fix ManhwaHentai download button for certain books

--- app/src/main/java/me/devsaki/hentoid/activities/sources/ManhwaActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/ManhwaActivity.java
@@ -5,7 +5,7 @@ import me.devsaki.hentoid.enums.Site;
 public class ManhwaActivity extends BaseWebActivity {
 
     private static final String DOMAIN_FILTER = "manhwahentai.me";
-    private static final String[] GALLERY_FILTER = {"//manhwahentai.me/[\\w\\-]+/[\\w\\-]{3,}/$"};
+    private static final String[] GALLERY_FILTER = {"//manhwahentai.me/[%\\w\\-]+/[%\\w\\-]{3,}/$"};
     private static final String[] DIRTY_ELEMENTS = {".c-ads"};
     private static final String[] JS_CONTENT_BLACKLIST = {"'iframe'","'adsdomain'","'closead'"};
     private static final String[] BLOCKED_CONTENT = {".cloudfront.net"};
