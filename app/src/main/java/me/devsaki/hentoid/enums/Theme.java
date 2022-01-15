8259db7af623f59d4f358dddc8fd1baa725486ec
Robb <35880555+robbwatershed@users.noreply.github.com>
2022-01-15 11:59:32 +0100

    Documentation

--- app/src/main/java/me/devsaki/hentoid/enums/Theme.java
+++ app/src/main/java/me/devsaki/hentoid/enums/Theme.java
@@ -9,8 +9,8 @@ public enum Theme {
 
     // Warning : names are _resoure names_ that should match the xml files, not display names
     LIGHT(0, "Light"),
-    DARK(1, "Dark"),
-    BLACK(2, "Black"),
+    DARK(1, "Dark"), // "Deep red" in settings
+    BLACK(2, "Black"), // "Dark" in settings, not to be mistaken with usual "black" that means AMOLED
     NONE(99, "Light");
 
     private final int id;
