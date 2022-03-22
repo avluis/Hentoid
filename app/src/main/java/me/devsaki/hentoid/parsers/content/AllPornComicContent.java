e982a9cafc78bf714312a03c403178aedf727f65
Robb <35880555+robbwatershed@users.noreply.github.com>
2022-03-22 21:56:22 +0100

    Save upload date for AllPornComic

--- app/src/main/java/me/devsaki/hentoid/parsers/content/AllPornComicContent.java
+++ app/src/main/java/me/devsaki/hentoid/parsers/content/AllPornComicContent.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 
 import org.jsoup.nodes.Element;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,15 +15,21 @@ import me.devsaki.hentoid.database.domains.Content;
 import me.devsaki.hentoid.enums.AttributeType;
 import me.devsaki.hentoid.enums.Site;
 import me.devsaki.hentoid.enums.StatusContent;
+import me.devsaki.hentoid.json.sources.YoastGalleryMetadata;
 import me.devsaki.hentoid.parsers.ParseHelper;
+import me.devsaki.hentoid.util.Helper;
+import me.devsaki.hentoid.util.JsonHelper;
 import me.devsaki.hentoid.util.StringHelper;
 import pl.droidsonroids.jspoon.annotation.Selector;
+import timber.log.Timber;
 
 public class AllPornComicContent extends BaseContentParser {
     @Selector(value = "head [property=og:image]", attr = "content", defValue = "")
     private String coverUrl;
     @Selector(value = "head [property=og:title]", attr = "content", defValue = "")
     private String title;
+    @Selector(value = "head script.yoast-schema-graph")
+    private Element metadata;
 
     @Selector(value = ".post-content a[href*='characters']")
     private List<Element> characterTags;
@@ -44,6 +51,17 @@ public class AllPornComicContent extends BaseContentParser {
         if (!title.isEmpty()) {
             content.setTitle(StringHelper.removeNonPrintableChars(title));
         } else content.setTitle(NO_TITLE);
+
+        if (metadata != null && metadata.childNodeSize() > 0) {
+            try {
+                YoastGalleryMetadata galleryMeta = JsonHelper.jsonToObject(metadata.childNode(0).toString(), YoastGalleryMetadata.class);
+                String publishDate = galleryMeta.getDatePublished(); // e.g. 2021-01-27T15:20:38+00:00
+                if (!publishDate.isEmpty())
+                    content.setUploadDate(Helper.parseDatetimeToEpoch(publishDate, "yyyy-MM-dd'T'HH:mm:ssXXX"));
+            } catch (IOException e) {
+                Timber.i(e);
+            }
+        }
 
         AttributeMap attributes = new AttributeMap();
         ParseHelper.parseAttributes(attributes, AttributeType.CHARACTER, characterTags, false, Site.ALLPORNCOMIC);
