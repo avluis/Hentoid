18fba551b28fd64f118f70dd552d0ba2c8464cda
Robb <35880555+robbwatershed@users.noreply.github.com>
2022-04-02 13:27:58 +0200

    Make Github API URL a Gradle constant

--- app/src/main/java/me/devsaki/hentoid/retrofit/GithubServer.kt
+++ app/src/main/java/me/devsaki/hentoid/retrofit/GithubServer.kt
@@ -3,6 +3,7 @@ package me.devsaki.hentoid.retrofit
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import io.reactivex.Single
+import me.devsaki.hentoid.BuildConfig
 import me.devsaki.hentoid.json.GithubRelease
 import me.devsaki.hentoid.util.network.OkHttpClientSingleton
 import retrofit2.Retrofit
@@ -12,8 +13,6 @@ import retrofit2.http.GET
 import java.util.*
 
 object GithubServer {
-
-    private const val GITHUB_BASE_URL = "https://api.github.com/repos/avluis/Hentoid/"
 
     private val moshi: Moshi by lazy {
         Moshi.Builder()
@@ -29,7 +28,7 @@ object GithubServer {
 
     fun init() {
         api = Retrofit.Builder()
-            .baseUrl(GITHUB_BASE_URL)
+            .baseUrl(BuildConfig.GITHUB_API_URL)
             .client(OkHttpClientSingleton.getInstance())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
             .addConverterFactory(MoshiConverterFactory.create(moshi).asLenient())
