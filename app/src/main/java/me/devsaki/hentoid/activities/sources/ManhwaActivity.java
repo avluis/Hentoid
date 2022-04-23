9f13054fabf20b947e2dc82e199f2543248cded5
Robb <35880555+robbwatershed@users.noreply.github.com>
2022-04-23 18:56:05 +0200

    ManhwaHentai : Allow the downloading of one single chapter [#766]

--- app/src/main/java/me/devsaki/hentoid/activities/sources/ManhwaActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/ManhwaActivity.java
@@ -4,10 +4,12 @@ import me.devsaki.hentoid.enums.Site;
 
 public class ManhwaActivity extends BaseWebActivity {
 
+    public static final String GALLERY_PATTERN = "//manhwahentai.me/[%\\w\\-]+/[%\\w\\-]{3,}/$";
+
     private static final String DOMAIN_FILTER = "manhwahentai.me";
-    private static final String[] GALLERY_FILTER = {"//manhwahentai.me/[%\\w\\-]+/[%\\w\\-]{3,}/$"};
+    private static final String[] GALLERY_FILTER = {GALLERY_PATTERN, GALLERY_PATTERN.replace("$", "") + "ch[%\\w]+-[0-9]+/$"};
     private static final String[] DIRTY_ELEMENTS = {".c-ads"};
-    private static final String[] JS_CONTENT_BLACKLIST = {"'iframe'","'adsdomain'","'closead'"};
+    private static final String[] JS_CONTENT_BLACKLIST = {"'iframe'", "'adsdomain'", "'closead'"};
     private static final String[] BLOCKED_CONTENT = {".cloudfront.net"};
 
 
