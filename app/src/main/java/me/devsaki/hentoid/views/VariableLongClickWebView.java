dcbecc1efa2e5902e704b72271bce782a8bbb653
Robb <35880555+robbwatershed@users.noreply.github.com>
2022-09-04 11:23:04 +0200

    Use Debouncer instead of Message

--- app/src/main/java/me/devsaki/hentoid/views/VariableLongClickWebView.java
+++ app/src/main/java/me/devsaki/hentoid/views/VariableLongClickWebView.java
@@ -1,56 +1,48 @@
 package me.devsaki.hentoid.views;
 
 import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.webkit.WebView;
 
 import com.annimon.stream.function.BiConsumer;
 
+import me.devsaki.hentoid.util.Debouncer;
+
 
 /**
  * WebView implementation which allows setting arbitrary thresholds long clicks
  */
 public class VariableLongClickWebView extends WebView {
-    // The minimum duration to hold down a click to register as a long click
-    // (in ms). Default is 500ms (This is the same as the android system's
-    // default).
+    // The minimum duration to hold down a click to register as a long click (in ms).
+    // Default is 500ms (This is the same as the android system's default).
     private int longClickThreshold = 500;
 
-    private Handler handler;
-    private static final int MESSAGE_LONG_CLICK = 1;
-    private int tapX;
-    private int tapY;
+    private Debouncer<Point> longTapDebouncer;
 
     private BiConsumer<Integer, Integer> onLongClickListener;
 
     public VariableLongClickWebView(final Context context) {
         super(context);
-        init();
+        init(longClickThreshold);
     }
 
     public VariableLongClickWebView(final Context context, final AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(longClickThreshold);
     }
 
     public VariableLongClickWebView(final Context context, final AttributeSet attrs, final int defStyle) {
         super(context, attrs, defStyle);
-        init();
+        init(longClickThreshold);
     }
 
-    private void init() {
-        handler = new Handler(Looper.getMainLooper(), message -> {
-            if (message.what == MESSAGE_LONG_CLICK && onLongClickListener != null) {
-//                super.setOnLongClickListener(null);
-//                performLongClick();
-//                super.setOnLongClickListener(onLongClickListener);
-                onLongClickListener.accept(tapX, tapY);
+    private void init(long longTapDebouncerThreshold) {
+        longTapDebouncer = new Debouncer<>(getContext(), longTapDebouncerThreshold, point -> {
+            if (onLongClickListener != null) {
+                onLongClickListener.accept(point.x, point.y);
             }
-            return true;
         });
     }
 
@@ -60,6 +52,7 @@ public class VariableLongClickWebView extends WebView {
 
     public void setLongClickThreshold(int threshold) {
         longClickThreshold = threshold;
+        init(threshold);
     }
 
     @Override
@@ -69,17 +62,13 @@ public class VariableLongClickWebView extends WebView {
         switch (action) {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_POINTER_DOWN:
-                Message m = new Message();
-                m.what = MESSAGE_LONG_CLICK;
-                tapX = (int) event.getX();
-                tapY = (int) event.getY();
-                handler.sendMessageDelayed(m, longClickThreshold);
+                longTapDebouncer.submit(new Point((int) event.getX(), (int) event.getY()));
                 break;
 
             case MotionEvent.ACTION_POINTER_UP:
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
-                handler.removeMessages(MESSAGE_LONG_CLICK);
+                longTapDebouncer.clear();
                 break;
 
             default:
