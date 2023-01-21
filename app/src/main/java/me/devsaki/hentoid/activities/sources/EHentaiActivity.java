a84a64763b732e7b652d125ed2cdcc7bae3782ff
Robb <35880555+robbwatershed@users.noreply.github.com>
2023-01-21 15:51:26 +0100

    Warn user when browsing e-h while not being logged in, when hi-res downloads are enabled

--- app/src/main/java/me/devsaki/hentoid/activities/sources/EHentaiActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/EHentaiActivity.java
@@ -1,8 +1,10 @@
 package me.devsaki.hentoid.activities.sources;
 
+import android.graphics.Bitmap;
 import android.webkit.CookieManager;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +14,12 @@ import java.util.Map;
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
+import me.devsaki.hentoid.R;
 import me.devsaki.hentoid.enums.Site;
 import me.devsaki.hentoid.parsers.content.ContentParser;
 import me.devsaki.hentoid.parsers.content.EhentaiContent;
+import me.devsaki.hentoid.parsers.images.EHentaiParser;
+import me.devsaki.hentoid.util.Preferences;
 import timber.log.Timber;
 
 /**
@@ -40,10 +45,21 @@ public class EHentaiActivity extends BaseWebActivity {
         return client;
     }
 
-    private static class EHentaiWebClient extends CustomWebViewClient {
+    private class EHentaiWebClient extends CustomWebViewClient {
 
         EHentaiWebClient(Site site, String[] filter, CustomWebActivity activity) {
             super(site, filter, activity);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+
+            EHentaiParser.EhAuthState authState = EHentaiParser.getAuthState(url);
+            if (Preferences.isDownloadEhHires() && authState != EHentaiParser.EhAuthState.LOGGED && !url.startsWith("https://forums.e-hentai.org/index.php")) {
+                webView.loadUrl("https://forums.e-hentai.org/index.php?act=Login&CODE=00/");
+                showTooltip(R.string.help_web_hires_eh_account, true);
+            }
         }
 
         // We call the API without using BaseWebActivity.parseResponse
