953f241440acb8850cbd0e883f32669eb4c1d7a8
Robb <35880555+robbwatershed@users.noreply.github.com>
2023-04-21 09:53:44 +0200

    Edoujin : Remove ads

--- app/src/main/java/me/devsaki/hentoid/activities/sources/EdoujinActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/EdoujinActivity.java
@@ -9,6 +9,9 @@ public class EdoujinActivity extends BaseWebActivity {
     private static final String DOMAIN_FILTER = "edoujin.net";
     private static final String[] GALLERY_FILTER = {GALLERY_PATTERN, GALLERY_PATTERN.replace("/$", "\\-") + "[0-9]+/$"};
 
+    private static final String[] JS_WHITELIST = {DOMAIN_FILTER};
+    private static final String[] JS_CONTENT_BLACKLIST = {"exoloader", "popunder"};
+
     Site getStartSite() {
         return Site.EDOUJIN;
     }
@@ -17,6 +20,8 @@ public class EdoujinActivity extends BaseWebActivity {
     protected CustomWebViewClient createWebClient() {
         CustomWebViewClient client = new CustomWebViewClient(getStartSite(), GALLERY_FILTER, this);
         client.restrictTo(DOMAIN_FILTER);
+        client.addJavascriptBlacklist(JS_CONTENT_BLACKLIST);
+        client.adBlocker.addToJsUrlWhitelist(JS_WHITELIST);
         return client;
     }
 }
