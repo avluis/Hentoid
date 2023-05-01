5266c3bbac0b323464f5c9d265bf84035e3b84b8
Robb <35880555+robbwatershed@users.noreply.github.com>
2023-05-01 16:16:26 +0200

    Optimize memory consumption : create a Bitmap object as late as possible

--- app/src/main/java/me/devsaki/hentoid/util/image/ImageTransform.kt
+++ app/src/main/java/me/devsaki/hentoid/util/image/ImageTransform.kt
@@ -2,6 +2,7 @@ package me.devsaki.hentoid.util.image
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Point
 import android.os.Build
 import com.squareup.moshi.JsonClass
 import me.devsaki.hentoid.core.HentoidApp
@@ -25,12 +26,13 @@ object ImageTransform {
         val transcoderLossy: PictureEncoder,
         val transcoderLossless: PictureEncoder,
         val transcodeQuality: Int,
-        @Transient
-        var forceManhwa: Boolean = false
+        @Transient var forceManhwa: Boolean = false
     )
 
     val screenWidth: Int = HentoidApp.getInstance().resources.displayMetrics.widthPixels
     val screenHeight: Int = HentoidApp.getInstance().resources.displayMetrics.heightPixels
+
+    private const val MAX_WEBP_DIMENSION = 16383
 
     /**
      * Transform the given raw picture data using the given params
@@ -38,62 +40,70 @@ object ImageTransform {
     fun transform(source: ByteArray, params: Params): ByteArray {
         if (ImageHelper.isImageAnimated(source)) return source
 
-        var bitmapOut = BitmapFactory.decodeByteArray(source, 0, source.size)
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeByteArray(source, 0, source.size, options)
+        val dims = Point(options.outWidth, options.outHeight)
+        val bitmapOut: Bitmap
         if (params.resizeEnabled) {
-            when (params.resizeMethod) {
-                0 -> bitmapOut = resizeScreenRatio(bitmapOut, params.resize1Ratio / 100f)
-                1 -> bitmapOut = resizeDims(
-                    bitmapOut,
-                    params.resize2Height,
-                    params.resize2Width,
-                    params.forceManhwa
+            bitmapOut = when (params.resizeMethod) {
+                0 -> resizeScreenRatio(source, dims, params.resize1Ratio / 100f)
+                1 -> resizeDims(
+                    source, dims, params.resize2Height, params.resize2Width, params.forceManhwa
                 )
-                2 -> bitmapOut = resizePlainRatio(bitmapOut, params.resize3Ratio / 100f)
+
+                else -> resizePlainRatio(source, dims, params.resize3Ratio / 100f)
             }
+        } else {
+            bitmapOut = BitmapFactory.decodeByteArray(source, 0, source.size)
         }
         val isLossless = ImageHelper.isImageLossless(source)
-        return transcodeTo(bitmapOut, determineEncoder(isLossless, params), params.transcodeQuality)
+        try {
+            return transcodeTo(
+                bitmapOut, determineEncoder(isLossless, params), params.transcodeQuality
+            )
+        } finally {
+            bitmapOut.recycle()
+        }
     }
 
-    private fun resizeScreenRatio(bitmap: Bitmap, ratio: Float): Bitmap {
+    private fun resizeScreenRatio(source: ByteArray, dims: Point, ratio: Float): Bitmap {
         val targetWidth = screenWidth * ratio
         val targetHeight = screenHeight * ratio
-        val widthRatio = targetWidth / bitmap.width
-        val heightRatio = targetHeight / bitmap.height
-        val targetRatio =
-            if (widthRatio > 1 && heightRatio > 1) max(widthRatio, heightRatio)
-            else min(widthRatio, heightRatio)
-        return resizePlainRatio(bitmap, targetRatio)
+        val widthRatio = targetWidth / dims.x
+        val heightRatio = targetHeight / dims.y
+        val targetRatio = if (widthRatio > 1 && heightRatio > 1) max(widthRatio, heightRatio)
+        else min(widthRatio, heightRatio)
+        return resizePlainRatio(source, dims, targetRatio)
     }
 
     private fun resizeDims(
-        bitmap: Bitmap,
-        maxHeight: Int,
-        maxWidth: Int,
-        forceManhwa: Boolean
+        source: ByteArray, dims: Point, maxHeight: Int, maxWidth: Int, forceManhwa: Boolean
     ): Bitmap {
-        val isManhwa = forceManhwa || (bitmap.height * 1.0 / bitmap.width > 3)
+        val isManhwa = forceManhwa || (dims.y * 1.0 / dims.x > 3)
         val ratio = if (isManhwa) {
-            if (bitmap.width > maxWidth) maxWidth * 1f / bitmap.width else 1f
+            if (dims.x > maxWidth) maxWidth * 1f / dims.x else 1f
         } else {
-            val maxDim = max(bitmap.width, bitmap.height) // Portrait vs. landscape
+            val maxDim = max(dims.x, dims.y) // Portrait vs. landscape
             if (maxDim > maxHeight) maxHeight * 1f / maxDim else 1f
         }
-        return resizePlainRatio(bitmap, ratio)
+        return resizePlainRatio(source, dims, ratio)
     }
 
-    private fun resizePlainRatio(source: Bitmap, ratio: Float): Bitmap {
+    private fun resizePlainRatio(source: ByteArray, dims: Point, ratio: Float): Bitmap {
         // TODO use upscaler if needed
-        return if (ratio > 0.99 && ratio < 1.01) source
+        val sourceBmp = BitmapFactory.decodeByteArray(source, 0, source.size)
+        return if (ratio > 0.99 && ratio < 1.01) sourceBmp
         else {
-            val rescaled = ImageHelper.sharpRescale(source, ratio)
-            val final = Bitmap.createScaledBitmap(
-                rescaled,
-                (source.width * ratio).toInt(),
-                (source.height * ratio).toInt(), true
-            )
-            if (rescaled != source) rescaled.recycle()
-            return final
+            val rescaled = ImageHelper.sharpRescale(sourceBmp, ratio)
+            if (rescaled != sourceBmp) sourceBmp.recycle()
+            try {
+                Bitmap.createScaledBitmap(
+                    rescaled, (dims.x * ratio).toInt(), (dims.y * ratio).toInt(), true
+                )
+            } finally {
+                rescaled.recycle()
+            }
         }
     }
 
@@ -107,23 +117,23 @@ object ImageTransform {
     private fun transcodeTo(bitmap: Bitmap, encoder: PictureEncoder, quality: Int): ByteArray {
         val output = ByteArrayOutputStream()
         when (encoder) {
-            PictureEncoder.WEBP_LOSSY ->
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
-                    bitmap.compress(Bitmap.CompressFormat.WEBP_LOSSY, quality, output)
-                else
-                    bitmap.compress(Bitmap.CompressFormat.WEBP, quality, output)
+            PictureEncoder.WEBP_LOSSY -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) bitmap.compress(
+                Bitmap.CompressFormat.WEBP_LOSSY,
+                quality,
+                output
+            )
+            else bitmap.compress(Bitmap.CompressFormat.WEBP, quality, output)
 
-            PictureEncoder.WEBP_LOSSLESS ->
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
-                    bitmap.compress(Bitmap.CompressFormat.WEBP_LOSSLESS, 100, output)
-                else
-                    bitmap.compress(Bitmap.CompressFormat.WEBP, 100, output)
+            PictureEncoder.WEBP_LOSSLESS -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) bitmap.compress(
+                Bitmap.CompressFormat.WEBP_LOSSLESS,
+                100,
+                output
+            )
+            else bitmap.compress(Bitmap.CompressFormat.WEBP, 100, output)
 
-            PictureEncoder.PNG ->
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, output)
+            PictureEncoder.PNG -> bitmap.compress(Bitmap.CompressFormat.PNG, 100, output)
 
-            PictureEncoder.JPEG ->
-                bitmap.compress(Bitmap.CompressFormat.JPEG, quality, output)
+            PictureEncoder.JPEG -> bitmap.compress(Bitmap.CompressFormat.JPEG, quality, output)
         }
         return output.toByteArray()
     }
