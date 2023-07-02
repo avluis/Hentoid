eb4f5a516980ebd6c7519155a170fd03c180ec17
Robb <35880555+robbwatershed@users.noreply.github.com>
2023-07-02 17:05:48 +0200

    Limit Glide parallel processing to 4 threads

--- app/src/main/java/me/devsaki/hentoid/core/CustomGlideModule.java
+++ app/src/main/java/me/devsaki/hentoid/core/CustomGlideModule.java
@@ -1,13 +1,16 @@
 package me.devsaki.hentoid.core;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.engine.executor.GlideExecutor;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
 
@@ -17,7 +20,7 @@ import me.devsaki.hentoid.util.network.OkHttpClientSingleton;
 import okhttp3.OkHttpClient;
 
 /**
- * Startup class to enable Glide over OkHttp
+ * Startup class to enable Glide over OkHttp and limit Glide parallel threads
  */
 @GlideModule
 public final class CustomGlideModule extends AppGlideModule {
@@ -29,5 +32,17 @@ public final class CustomGlideModule extends AppGlideModule {
         OkHttpUrlLoader.Factory factory = new OkHttpUrlLoader.Factory(client);
 
         registry.replace(GlideUrl.class, InputStream.class, factory);
+    }
+
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        builder                                // customize builder
+                .setLogLevel(Log.ERROR)
+                .setSourceExecutor(                // source executor specify the thread it uses to load the image from remote
+                        GlideExecutor
+                                .newSourceBuilder()
+                                .setThreadCount(4)         // use the builder to set a desired thread amount
+                                .build()
+                );
     }
 }
