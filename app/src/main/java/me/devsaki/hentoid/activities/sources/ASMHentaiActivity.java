c8557bece028ddf3ef1db3e2f041e6144e995a49
Robb <35880555+robbwatershed@users.noreply.github.com>
2023-09-17 20:16:25 +0200

    ASM : Remove ads

--- app/src/main/java/me/devsaki/hentoid/activities/sources/ASMHentaiActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/ASMHentaiActivity.java
@@ -9,7 +9,10 @@ public class ASMHentaiActivity extends BaseWebActivity {
 
     private static final String DOMAIN_FILTER = "asmhentai.com";
     private static final String[] GALLERY_FILTER = {"asmhentai.com/g/"};
+    private static final String[] REMOVABLE_ELEMENTS = {".atop"};
     private static final String[] blockedContent = {"f.js"};
+    private static final String[] JS_WHITELIST = {DOMAIN_FILTER};
+    private static final String[] JS_CONTENT_BLACKLIST = {"data-ad", "exoloader", "popunder", "close ad"};
 
     Site getStartSite() {
         return Site.ASMHENTAI;
@@ -20,7 +23,10 @@ public class ASMHentaiActivity extends BaseWebActivity {
     protected CustomWebViewClient createWebClient() {
         CustomWebViewClient client = new CustomWebViewClient(getStartSite(), GALLERY_FILTER, this);
         client.restrictTo(DOMAIN_FILTER);
+        client.addRemovableElements(REMOVABLE_ELEMENTS);
         client.adBlocker.addToUrlBlacklist(blockedContent);
+        client.adBlocker.addToJsUrlWhitelist(JS_WHITELIST);
+        for (String s : JS_CONTENT_BLACKLIST) client.adBlocker.addJsContentBlacklist(s);
         return client;
     }
 }
