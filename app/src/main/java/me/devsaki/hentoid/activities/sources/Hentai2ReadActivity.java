59b488fedc6938215f1962b8d47ecc7003406a80
Robb <35880555+robbwatershed@users.noreply.github.com>
2023-10-22 19:22:43 +0200

    H2R : Remove ads

--- app/src/main/java/me/devsaki/hentoid/activities/sources/Hentai2ReadActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/Hentai2ReadActivity.java
@@ -8,9 +8,9 @@ public class Hentai2ReadActivity extends BaseWebActivity {
 
     private static final String DOMAIN_FILTER = "hentai2read.com";
     private static final String[] GALLERY_FILTER = {GALLERY_PATTERN, GALLERY_PATTERN.replace("$", "") + "[0-9\\.]+/$"};
-    private static final String[] REMOVABLE_ELEMENTS = {"div[data-refresh]"}; // iframe[src*=ads]
+    private static final String[] REMOVABLE_ELEMENTS = {"div[data-refresh]", ".js-rotating"}; // iframe[src*=ads]
     private static final String[] JS_WHITELIST = {DOMAIN_FILTER};
-    private static final String[] JS_CONTENT_BLACKLIST = {"exoloader", "popunder", "trackingurl", "exo_slider", "exojspop", "data-exo"};
+    private static final String[] JS_CONTENT_BLACKLIST = {"exoloader", "popunder", "trackingurl", "exo_slider", "exojspop", "data-exo", "exoslider"};
 
 
     Site getStartSite() {
@@ -22,6 +22,7 @@ public class Hentai2ReadActivity extends BaseWebActivity {
         CustomWebViewClient client = new CustomWebViewClient(getStartSite(), GALLERY_FILTER, this);
         client.restrictTo(DOMAIN_FILTER);
         client.addRemovableElements(REMOVABLE_ELEMENTS);
+        client.addJavascriptBlacklist(JS_CONTENT_BLACKLIST);
         client.adBlocker.addToJsUrlWhitelist(JS_WHITELIST);
         for (String s : JS_CONTENT_BLACKLIST) client.adBlocker.addJsContentBlacklist(s);
         return client;
