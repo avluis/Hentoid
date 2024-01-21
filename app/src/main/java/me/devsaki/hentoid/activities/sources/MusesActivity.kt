fc21533fc0a90160ea911f38425692c436a9c9ce
Robb <35880555+robbwatershed@users.noreply.github.com>
2024-01-21 09:28:33 +0100

    Muses : Remove ads

--- app/src/main/java/me/devsaki/hentoid/activities/sources/MusesActivity.kt
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/MusesActivity.kt
@@ -9,6 +9,7 @@ class MusesActivity : BaseWebActivity() {
         private val GALLERY_FILTER =
             arrayOf("//www.8muses.com/comics/album/", "//comics.8muses.com/comics/album/")
         // private static final String[] REMOVABLE_ELEMENTS = {".c-tile:not([href])"}; // <-- even when removing empty tiles, ads are generated and force-inserted by the ad JS (!)
+        private val JS_CONTENT_BLACKLIST = arrayOf("exoloader", "popunder")
     }
 
     override fun getStartSite(): Site {
@@ -18,6 +19,7 @@ class MusesActivity : BaseWebActivity() {
     override fun createWebClient(): CustomWebViewClient {
         val client = CustomWebViewClient(getStartSite(), GALLERY_FILTER, this)
         client.restrictTo(DOMAIN_FILTER)
+        for (s in JS_CONTENT_BLACKLIST) client.adBlocker.addJsContentBlacklist(s)
         return client
     }
 }
