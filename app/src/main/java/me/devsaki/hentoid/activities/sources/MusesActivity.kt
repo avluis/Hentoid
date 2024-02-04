6f6391145ce51a06849feea80a4e0116adf0302d
Robb <35880555+robbwatershed@users.noreply.github.com>
2024-02-04 10:53:27 +0100

    8Muses : Fix adblocker blocking the entire site

--- app/src/main/java/me/devsaki/hentoid/activities/sources/MusesActivity.kt
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/MusesActivity.kt
@@ -18,6 +18,7 @@ class MusesActivity : BaseWebActivity() {
     override fun createWebClient(): CustomWebViewClient {
         val client = CustomWebViewClient(getStartSite(), GALLERY_FILTER, this)
         client.restrictTo(DOMAIN_FILTER)
+        client.adBlocker.addToJsUrlWhitelist(DOMAIN_FILTER)
         return client
     }
 }
