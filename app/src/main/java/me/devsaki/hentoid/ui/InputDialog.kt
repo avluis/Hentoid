baccfed7453e149e7affde1e5c84dda5a2be5e9e
Robb <35880555+robbwatershed@users.noreply.github.com>
2024-02-04 22:44:40 +0100

    Better UI for inputDialog; clear button on text inputs

--- app/src/main/java/me/devsaki/hentoid/ui/InputDialog.kt
+++ app/src/main/java/me/devsaki/hentoid/ui/InputDialog.kt
@@ -7,10 +7,12 @@ import android.text.InputFilter
 import android.text.InputFilter.LengthFilter
 import android.text.InputType
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
 import androidx.annotation.StringRes
 import androidx.core.view.inputmethod.EditorInfoCompat
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.textfield.TextInputLayout.END_ICON_CLEAR_TEXT
 import me.devsaki.hentoid.R
 import me.devsaki.hentoid.core.Consumer
 
@@ -21,24 +23,27 @@ object InputDialog {
         @StringRes message: Int,
         onResult: Consumer<Int>
     ) {
-        val input = EditText(context)
-        input.inputType = InputType.TYPE_CLASS_NUMBER
-        input.setRawInputType(Configuration.KEYBOARD_12KEY)
-        input.filters =
-            arrayOf<InputFilter>(LengthFilter(9)) // We don't expect any number longer than 9 chars (999 million)
-        val onOk = DialogInterface.OnClickListener { _, _ ->
-            if (input.text.isNotEmpty()) onResult.invoke(input.text.toString().toInt())
-            val imm =
-                context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(input.windowToken, 0)
-        }
-        val onCancel =
-            DialogInterface.OnClickListener { _, _ ->
+        val layout = TextInputLayout(context)
+        layout.addView(TextInputEditText(context))
+        layout.editText?.let { input ->
+            input.inputType = InputType.TYPE_CLASS_NUMBER
+            input.setRawInputType(Configuration.KEYBOARD_12KEY)
+            input.filters =
+                arrayOf<InputFilter>(LengthFilter(9)) // We don't expect any number longer than 9 chars (999 million)
+            val onOk = DialogInterface.OnClickListener { _, _ ->
+                if (input.text.isNotEmpty()) onResult.invoke(input.text.toString().toInt())
                 val imm =
                     context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 imm.hideSoftInputFromWindow(input.windowToken, 0)
             }
-        showDialog(context, message, input, onOk, onCancel)
+            val onCancel =
+                DialogInterface.OnClickListener { _, _ ->
+                    val imm =
+                        context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(input.windowToken, 0)
+                }
+            showDialog(context, message, layout, onOk, onCancel)
+        }
     }
 
     fun invokeInputDialog(
@@ -48,48 +53,53 @@ object InputDialog {
         text: String = "",
         onCancelled: Runnable? = null
     ) {
-        val input = EditText(context)
-        if (text.isNotEmpty()) input.setText(text)
-        input.inputType = InputType.TYPE_CLASS_TEXT
-        input.imeOptions = EditorInfoCompat.IME_FLAG_NO_PERSONALIZED_LEARNING
-        val onOk = DialogInterface.OnClickListener { _, _ ->
-            if (input.text.isNotEmpty()) onResult.invoke(
-                input.text.toString().trim { it <= ' ' })
-            val imm =
-                context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(input.windowToken, 0)
-        }
-        val onCancel =
-            DialogInterface.OnClickListener { _, _ ->
+        val layout = TextInputLayout(context)
+        layout.addView(TextInputEditText(context))
+        layout.endIconMode = END_ICON_CLEAR_TEXT
+        layout.editText?.let { input ->
+            if (text.isNotEmpty()) input.setText(text)
+            input.inputType = InputType.TYPE_CLASS_TEXT
+            input.imeOptions = EditorInfoCompat.IME_FLAG_NO_PERSONALIZED_LEARNING
+            val onOk = DialogInterface.OnClickListener { _, _ ->
+                if (input.text.isNotEmpty()) onResult.invoke(input.text.toString().trim())
                 val imm =
                     context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 imm.hideSoftInputFromWindow(input.windowToken, 0)
-                onCancelled?.run()
             }
-        showDialog(context, message, input, onOk, onCancel)
+            val onCancel =
+                DialogInterface.OnClickListener { _, _ ->
+                    val imm =
+                        context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(input.windowToken, 0)
+                    onCancelled?.run()
+                }
+            showDialog(context, message, layout, onOk, onCancel)
+        }
     }
 
     private fun showDialog(
         context: Context,
         @StringRes message: Int,
-        input: EditText,
+        layout: TextInputLayout,
         onOk: DialogInterface.OnClickListener,
         onCancel: DialogInterface.OnClickListener
     ) {
         val materialDialog = MaterialAlertDialogBuilder(context)
-            .setView(input)
+            .setView(layout)
             .setMessage(message)
             .setPositiveButton(R.string.ok, onOk)
             .setNegativeButton(R.string.cancel, onCancel)
             .create()
         materialDialog.show()
-        input.requestFocus()
-        input.postDelayed(
-            {
-                val imm =
-                    context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.showSoftInput(input, 0)
-            }, 50
-        )
+        layout.editText?.let {
+            it.requestFocus()
+            it.postDelayed(
+                {
+                    val imm =
+                        context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.showSoftInput(it, 0)
+                }, 50
+            )
+        }
     }
 }
