7ed4184dbb5a3de57e98335bf6002fe98964aa20
Robb <35880555+robbwatershed@users.noreply.github.com>
2024-06-15 18:14:13 +0200

    Cleanup

--- app/src/main/java/me/devsaki/hentoid/util/file/FileFigures.kt
+++ app/src/main/java/me/devsaki/hentoid/util/file/FileFigures.kt
@@ -12,7 +12,7 @@ const val ONE_KB: Long = 1024
 /**
  * The number of bytes in a kilobyte.
  */
-val ONE_KB_BI = BigInteger.valueOf(ONE_KB)
+val ONE_KB_BI: BigInteger = BigInteger.valueOf(ONE_KB)
 
 /**
  * The number of bytes in a megabyte.
@@ -22,7 +22,7 @@ const val ONE_MB = ONE_KB * ONE_KB
 /**
  * The number of bytes in a megabyte.
  */
-val ONE_MB_BI = ONE_KB_BI.multiply(ONE_KB_BI)
+val ONE_MB_BI: BigInteger = ONE_KB_BI.multiply(ONE_KB_BI)
 
 /**
  * The number of bytes in a gigabyte.
@@ -32,7 +32,7 @@ const val ONE_GB = ONE_KB * ONE_MB
 /**
  * The number of bytes in a gigabyte.
  */
-val ONE_GB_BI = ONE_KB_BI.multiply(ONE_MB_BI)
+val ONE_GB_BI: BigInteger = ONE_KB_BI.multiply(ONE_MB_BI)
 
 /**
  * The number of bytes in a terabyte.
@@ -42,7 +42,7 @@ const val ONE_TB = ONE_KB * ONE_GB
 /**
  * The number of bytes in a terabyte.
  */
-val ONE_TB_BI = ONE_KB_BI.multiply(ONE_GB_BI)
+val ONE_TB_BI: BigInteger = ONE_KB_BI.multiply(ONE_GB_BI)
 
 /**
  * The number of bytes in a petabyte.
@@ -52,7 +52,7 @@ const val ONE_PB = ONE_KB * ONE_TB
 /**
  * The number of bytes in a petabyte.
  */
-val ONE_PB_BI = ONE_KB_BI.multiply(ONE_TB_BI)
+val ONE_PB_BI: BigInteger = ONE_KB_BI.multiply(ONE_TB_BI)
 
 /**
  * The number of bytes in an exabyte.
@@ -62,14 +62,14 @@ const val ONE_EB = ONE_KB * ONE_PB
 /**
  * The number of bytes in an exabyte.
  */
-val ONE_EB_BI = ONE_KB_BI.multiply(ONE_PB_BI)
+val ONE_EB_BI: BigInteger = ONE_KB_BI.multiply(ONE_PB_BI)
 
 /**
  * The number of bytes in a zettabyte.
  */
-val ONE_ZB = BigInteger.valueOf(ONE_KB).multiply(BigInteger.valueOf(ONE_EB))
+val ONE_ZB: BigInteger = BigInteger.valueOf(ONE_KB).multiply(BigInteger.valueOf(ONE_EB))
 
 /**
  * The number of bytes in a yottabyte.
  */
-val ONE_YB = ONE_KB_BI.multiply(ONE_ZB)
+val ONE_YB: BigInteger = ONE_KB_BI.multiply(ONE_ZB)
