5f56a557c0e26ec53a9a5ec3e34f283c06d2c901
Robb <35880555+robbwatershed@users.noreply.github.com>
2024-06-16 10:47:49 +0200

    SiteHistory -> Kt

--- app/src/main/java/me/devsaki/hentoid/database/domains/SiteHistory.kt
+++ app/src/main/java/me/devsaki/hentoid/database/domains/SiteHistory.kt
@@ -1,39 +1,22 @@
-package me.devsaki.hentoid.database.domains;
+package me.devsaki.hentoid.database.domains
 
-import io.objectbox.annotation.Convert;
-import io.objectbox.annotation.Entity;
-import io.objectbox.annotation.Id;
-import me.devsaki.hentoid.enums.Site;
+import io.objectbox.annotation.Convert
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
+import me.devsaki.hentoid.enums.Site
+import me.devsaki.hentoid.enums.Site.SiteConverter
 
 /**
  * Site browsing history
  */
 @Entity
-public class SiteHistory {
-
+data class SiteHistory(
     @Id
-    public long id;
-    @Convert(converter = Site.SiteConverter.class, dbType = Long.class)
-    private Site site;
-    private String url; // Last
-
-    public SiteHistory() { // Required by ObjectBox when an alternate constructor exists
-    }
-
-    public SiteHistory(Site site, String url) {
-        this.site = site;
-        this.url = url;
-    }
-
-    public Site getSite() {
-        return site;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
+    var id: Long = 0,
+    @Convert(converter = SiteConverter::class, dbType = Long::class)
+    val site: Site = Site.NONE,
+    var url: String = ""
+) {
+    constructor() : this(Site.NONE, "")
+    constructor(site: Site, url: String) : this(0, site, url)
 }
