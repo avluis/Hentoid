5ec0fb6b5ca299119eaa814064e9b8556c25a13f
Robb <35880555+robbwatershed@users.noreply.github.com>
2024-10-26 16:07:31 +0200

    ImageDecoder -> Kt

--- app/customssiv/src/main/java/me/devsaki/hentoid/customssiv/decoder/ImageDecoder.kt
+++ app/customssiv/src/main/java/me/devsaki/hentoid/customssiv/decoder/ImageDecoder.kt
@@ -1,33 +1,28 @@
-package me.devsaki.hentoid.customssiv.decoder;
+package me.devsaki.hentoid.customssiv.decoder
 
-
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.net.Uri;
-
-import androidx.annotation.NonNull;
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
 
 /**
  * Interface for image decoding classes, allowing the default {@link android.graphics.BitmapFactory}
  * based on the Skia library to be replaced with a custom class.
  */
-public interface ImageDecoder {
-
+interface ImageDecoder {
     /**
      * Decode an image. The URI can be in one of the following formats:
-     * <br>
-     * File: <code>file:///scard/picture.jpg</code>
-     * <br>
-     * Asset: <code>file:///android_asset/picture.png</code>
-     * <br>
-     * Resource: <code>android.resource://com.example.app/drawable/picture</code>
+     * <br></br>
+     * File: `file:///scard/picture.jpg`
+     * <br></br>
+     * Asset: `file:///android_asset/picture.png`
+     * <br></br>
+     * Resource: `android.resource://com.example.app/drawable/picture`
      *
      * @param context Application context
      * @param uri URI of the image
      * @return the decoded bitmap
      * @throws Exception if decoding fails.
      */
-    @NonNull
-    Bitmap decode(Context context, @NonNull Uri uri) throws Exception;
-
+    @Throws(Exception::class)
+    fun decode(context: Context, uri: Uri): Bitmap
 }
