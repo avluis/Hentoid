a73d9a49b3372de02553009e0107478d5a1e55e6
Robb <35880555+robbwatershed@users.noreply.github.com>
2024-11-23 10:48:36 +0100

    Cleanup

--- app/customssiv/src/main/java/me/devsaki/hentoid/customssiv/util/ResizeBitmapHelper.kt
+++ app/customssiv/src/main/java/me/devsaki/hentoid/customssiv/util/ResizeBitmapHelper.kt
@@ -1,6 +1,8 @@
 package me.devsaki.hentoid.customssiv.util
 
 import android.graphics.Bitmap
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import me.devsaki.hentoid.gles_renderer.GPUImage
 import me.devsaki.hentoid.gles_renderer.filter.GPUImageFilter
 import me.devsaki.hentoid.gles_renderer.filter.GPUImageGaussianBlurFilter
@@ -13,26 +15,25 @@ import kotlin.math.roundToInt
 
 // Credits go to https://medium.com/@petrakeas/alias-free-resize-with-renderscript-5bf15a86ce3
 
-fun resizeBitmap(
+suspend fun resizeBitmap(
     glEsRenderer: GPUImage?,
     src: Bitmap,
     targetScale: Float
-): Pair<Bitmap?, Float> {
-    assertNonUiThread()
+): Pair<Bitmap?, Float> = withContext(Dispatchers.Default) {
     if (null == glEsRenderer) {
         val resizeParams = computeResizeParams(targetScale)
         Timber.d(">> resizing successively to scale %s", resizeParams.second)
-        return Pair(successiveResize(src, resizeParams.first), resizeParams.second)
+        Pair(successiveResize(src, resizeParams.first), resizeParams.second)
     } else {
         if (targetScale < 0.75 || (targetScale > 1.0 && targetScale < 1.55)) {
-            // Don't use resize nice above 0.75%; classic bilinear resize does the job well with more sharpness to the picture
-            return Pair(
+            // Don't use smooth resize above 0.75%; classic bilinear resize does the job well with more sharpness to the picture
+            Pair(
                 resizeGLES(glEsRenderer, src, targetScale, targetScale),
                 targetScale
             )
         } else {
             Timber.d(">> No resize needed; keeping raw image")
-            return Pair(src, 1f)
+            Pair(src, 1f)
         }
     }
 }
@@ -58,7 +59,7 @@ private fun computeResizeParams(targetScale: Float): Pair<Int, Float> {
     return Pair(nbResize, resultScale)
 }
 
-fun successiveResize(src: Bitmap, resizeNum: Int): Bitmap {
+private fun successiveResize(src: Bitmap, resizeNum: Int): Bitmap {
     if (0 == resizeNum) return src
 
     var srcWidth = src.width
@@ -75,7 +76,12 @@ fun successiveResize(src: Bitmap, resizeNum: Int): Bitmap {
     return output
 }
 
-fun resizeGLES(glEsRenderer: GPUImage, src: Bitmap?, xScale: Float, yScale: Float): Bitmap? {
+private fun resizeGLES(
+    glEsRenderer: GPUImage,
+    src: Bitmap?,
+    xScale: Float,
+    yScale: Float
+): Bitmap? {
     // Calculate gaussian's radius
     val sigma = (1 / xScale) / Math.PI.toFloat()
     // https://android.googlesource.com/platform/frameworks/rs/+/master/cpu_ref/rsCpuIntrinsicBlur.cpp
