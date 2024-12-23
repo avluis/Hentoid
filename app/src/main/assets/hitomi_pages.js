7a38701f736e6c5fba87eff5536d9619f38632c3
Robb <35880555+robbwatershed@users.noreply.github.com>
2024-12-23 21:36:20 +0100

    Hitomi : Fix downloads

--- app/src/main/assets/hitomi_pages.js
+++ app/src/main/assets/hitomi_pages.js
@@ -1,11 +1,12 @@
 $galleryInfo;
 var pages = new Array(galleryinfo['files'].length);
 for (var i = 0; i < galleryinfo['files'].length; i++) {
-    if (galleryinfo['files'][i]['haswebp']) {
+// TODO handle hasavif / hasjxl
+    //if (galleryinfo['files'][i]['haswebp']) {
         pages[i] = url_from_url_from_hash(galleryinfo['id'], galleryinfo['files'][i], 'webp', undefined, 'a');
-    } else {
-        pages[i] = url_from_url_from_hash(galleryinfo['id'], galleryinfo['files'][i]);
-    }
+    //} else {
+//        pages[i] = url_from_url_from_hash(galleryinfo['id'], galleryinfo['files'][i]);
+//    }
     // url_from_url_from_hash(galleryinfo['id'], galleryinfo['files'][i], 'avif', undefined, 'a');
 }
 JSON.stringify(pages);
