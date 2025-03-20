9ba78600f6c0a39576b3c7bad07d8eb989b79dff
Robb <35880555+robbwatershed@users.noreply.github.com>
2025-03-20 21:19:52 +0100

    Fix Hitomi

--- app/src/main/assets/hitomi_pages.js
+++ app/src/main/assets/hitomi_pages.js
@@ -3,7 +3,7 @@ var pages = new Array(galleryinfo['files'].length);
 for (var i = 0; i < galleryinfo['files'].length; i++) {
 // TODO handle hasavif / hasjxl
     //if (galleryinfo['files'][i]['haswebp']) {
-        pages[i] = url_from_url_from_hash(galleryinfo['id'], galleryinfo['files'][i], 'webp', undefined, 'a');
+        pages[i] = url_from_url_from_hash(galleryinfo['id'], galleryinfo['files'][i], 'webp');
     //} else {
 //        pages[i] = url_from_url_from_hash(galleryinfo['id'], galleryinfo['files'][i]);
 //    }
