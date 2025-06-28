834ef69992cdb1236d4bcb278bf2415c7fcc8ca2
Robb <35880555+robbwatershed@users.noreply.github.com>
2025-06-28 17:54:22 +0200

    Fix downloads on e-h / exH with multipage viewer enabled

--- app/src/main/java/me/devsaki/hentoid/json/sources/ehentai/EHentaiImageResponse.kt
+++ app/src/main/java/me/devsaki/hentoid/json/sources/ehentai/EHentaiImageResponse.kt
@@ -1,5 +1,7 @@
 package me.devsaki.hentoid.json.sources.ehentai
 
+import com.squareup.moshi.Json
+
 data class EHentaiImageResponse(
     // Relative link to the full-size image
     val fullUrlRelative: String? = null,
@@ -7,6 +9,7 @@ data class EHentaiImageResponse(
     val ll: String? = null,
     val lo: String? = null,
     // image displayed in the multipage viewer
+    @Json(name = "i")
     val url: String,
     val s: String? = null
 )
