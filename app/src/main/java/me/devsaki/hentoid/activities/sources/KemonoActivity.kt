666d9358a8dded0f6761b67ac158bd5123eeeb77
Robb <35880555+robbwatershed@users.noreply.github.com>
2025-07-27 10:52:01 +0200

    Kemono.su -> .cr (cont'ed)

--- app/src/main/java/me/devsaki/hentoid/activities/sources/KemonoActivity.kt
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/KemonoActivity.kt
@@ -12,12 +12,12 @@ import timber.log.Timber
 
 class KemonoActivity : BaseWebActivity() {
     companion object {
-        private const val DOMAIN_FILTER = "kemono.su"
+        private const val DOMAIN_FILTER = "kemono.cr"
         private val GALLERY_FILTER = arrayOf(
-            "kemono.su/[\\w_%\\-]+/user/[\\d\\-]+$",
-            "kemono.su/api/v1/[\\w_%\\-]+/user/[\\d\\-]+/posts-legacy$",
-            "kemono.su/[\\w_%\\-]+/user/[\\d\\-]+/post/[\\d\\-]+$",
-            "kemono.su/api/v1/[\\w_%\\-]+/user/[\\d\\-]+/post/[\\d\\-]+$"
+            "kemono.cr/[\\w_%\\-]+/user/[\\d\\-]+$",
+            "kemono.cr/api/v1/[\\w_%\\-]+/user/[\\d\\-]+/posts-legacy$",
+            "kemono.cr/[\\w_%\\-]+/user/[\\d\\-]+/post/[\\d\\-]+$",
+            "kemono.cr/api/v1/[\\w_%\\-]+/user/[\\d\\-]+/post/[\\d\\-]+$"
         )
         private val BLOCKED_CONTENT = arrayOf("popunder")
         private val REMOVABLE_ELEMENTS = arrayOf("section.advertisement")
