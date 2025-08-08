662f71fce62cb6f7abf82da237590396e425ab50
Robb <35880555+robbwatershed@users.noreply.github.com>
2025-08-08 15:10:25 +0200

    Fix concurrency crash

--- app/src/main/java/me/devsaki/hentoid/util/ProgressManager.kt
+++ app/src/main/java/me/devsaki/hentoid/util/ProgressManager.kt
@@ -1,9 +1,10 @@
 package me.devsaki.hentoid.util
 
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.math.max
 
 class ProgressManager(private val nbSteps: Int = 1) {
-    private val steps = HashMap<String, Float>()
+    private val steps = ConcurrentHashMap<String, Float>()
 
     fun setProgress(step: String, progress: Float) {
         steps[step] = progress
