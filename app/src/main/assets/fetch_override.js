ca9becdbccb743343f7f61fc61d94cd7ee1ca39f
Robb <35880555+robbwatershed@users.noreply.github.com>
2026-02-10 21:14:18 +0100

    Fix using fetchHandler on GET requests

--- app/src/main/assets/fetch_override.js
+++ app/src/main/assets/fetch_override.js
@@ -4,17 +4,23 @@ if (typeof origFetch === 'undefined') {
       /* console.log("fetch called with args:", args);*/
 
       /* Send the calling args to the app */
-      if (typeof (fetchHandler) != 'undefined')
-        fetchHandler.onFetchCall(args[0], args[1].body);
+      if (typeof (fetchHandler) != 'undefined') {
+        var body = ""
+        if (args.length > 1) body = args[1].body
+        fetchHandler.onFetchCall(args[0], body);
+      }
 
       const response = await origFetch(...args);
 
       /* Send the response to the app if handler is set */
-      if (typeof (fetchResponseHandler) != 'undefined')
-          response
+      if (typeof (fetchResponseHandler) != 'undefined') {
+         var body = ""
+         if (args.length > 1) body = args[1].body
+         response
             .clone()
-            .then(r => fetchResponseHandler.onCall(args[0], args[1].body, r.text()))
+            .then(r => fetchResponseHandler.onCall(args[0], body, r.text()))
             .catch(err => console.error(err));
+      }
 
       /* the original response is resolved unmodified */
       return response;
