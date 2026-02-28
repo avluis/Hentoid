b1bc18bb08dd8d75c6cec178f418f555f613b342
Robb <35880555+robbwatershed@users.noreply.github.com>
2026-02-28 13:51:41 +0100

    Bump ObjectBox

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version ("9.0.1") apply false
     id("com.google.gms.google-services") version ("4.4.1") apply false
-    id("io.objectbox") version "5.1.0" apply false
+    id("io.objectbox") version "5.2.0" apply false
     id("org.sonarqube") version "7.2.2.6593"
     id("com.google.firebase.crashlytics") version "3.0.6" apply false
     // Kept for ObjectBox while they still use kapt (see https://github.com/objectbox/objectbox-java/issues/1075)
